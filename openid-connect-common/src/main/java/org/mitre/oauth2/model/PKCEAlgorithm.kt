7890d750a768441a832d6ee298c664585995a50c
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-23 20:49:34 +0000

    Convert PKCEAlgorithm to Kotlin.

--- openid-connect-common/src/main/java/org/mitre/oauth2/model/PKCEAlgorithm.kt
+++ openid-connect-common/src/main/java/org/mitre/oauth2/model/PKCEAlgorithm.kt
@@ -5,53 +5,46 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.oauth2.model
 
-package org.mitre.oauth2.model;
-
-import com.nimbusds.jose.Algorithm;
-import com.nimbusds.jose.Requirement;
+import com.nimbusds.jose.Algorithm
+import com.nimbusds.jose.Requirement
 
 /**
  * @author jricher
- *
  */
-public final class PKCEAlgorithm extends Algorithm {
+class PKCEAlgorithm : Algorithm {
+    constructor(name: String?, req: Requirement?) : super(name, req)
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 7752852583210088925L;
+    constructor(name: String?) : super(name, null)
 
-	public static final PKCEAlgorithm plain = new PKCEAlgorithm("plain", Requirement.REQUIRED);
+    companion object {
+        /**
+         *
+         */
+        private const val serialVersionUID = 7752852583210088925L
 
-	public static final PKCEAlgorithm S256 = new PKCEAlgorithm("S256", Requirement.OPTIONAL);
+        @JvmField
+		val plain: PKCEAlgorithm = PKCEAlgorithm("plain", Requirement.REQUIRED)
 
-	public PKCEAlgorithm(String name, Requirement req) {
-		super(name, req);
-	}
+        @JvmField
+		val S256: PKCEAlgorithm = PKCEAlgorithm("S256", Requirement.OPTIONAL)
 
-	public PKCEAlgorithm(String name) {
-		super(name, null);
-	}
-
-	public static PKCEAlgorithm parse(final String s) {
-		if (s.equals(plain.getName())) {
-			return plain;
-		} else if (s.equals(S256.getName())) {
-			return S256;
-		} else {
-			return new PKCEAlgorithm(s);
-		}
-	}
-
-
-
+        @JvmStatic
+		fun parse(s: String): PKCEAlgorithm {
+            return when (s) {
+                plain.name -> plain
+                S256.name -> S256
+                else -> PKCEAlgorithm(s)
+            }
+        }
+    }
 }
