5a43ccd6478d963f00c9df22d63fd743eff9cda5
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-24 21:46:02 +0000

    Convert AuthRequestOptionsService to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/AuthRequestOptionsService.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/AuthRequestOptionsService.kt
@@ -7,22 +7,19 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service;
+ */
+package org.mitre.openid.connect.client.service
 
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.config.ServerConfiguration;
-
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.Map;
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.config.ServerConfiguration
+import javax.servlet.http.HttpServletRequest
 
 /**
  *
@@ -31,18 +28,23 @@ import java.util.Map;
  * These options may depend on the server configuration, client configuration, or HTTP request.
  *
  * @author jricher
- *
  */
-public interface AuthRequestOptionsService {
+interface AuthRequestOptionsService {
+    /**
+     * The set of options needed at the authorization endpoint.
+     */
+    fun getOptions(
+        server: ServerConfiguration,
+        client: RegisteredClient,
+        request: HttpServletRequest
+    ): Map<String, String>
 
-	/**
-	 * The set of options needed at the authorization endpoint.
-	 */
-	public Map<String, String> getOptions(ServerConfiguration server, RegisteredClient client, HttpServletRequest request);
-
-	/**
-	 * The set of options needed at the token endpoint.
-	 */
-	public Map<String, String> getTokenOptions(ServerConfiguration server, RegisteredClient client, HttpServletRequest request);
-
+    /**
+     * The set of options needed at the token endpoint.
+     */
+    fun getTokenOptions(
+        server: ServerConfiguration,
+        client: RegisteredClient,
+        request: HttpServletRequest
+    ): Map<String, String>
 }
