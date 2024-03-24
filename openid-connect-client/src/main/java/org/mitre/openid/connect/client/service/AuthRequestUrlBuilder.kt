3b617c42cd16203820e4079a02c5e23967008c8d
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-24 21:47:32 +0000

    Convert the service interfaces to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/AuthRequestUrlBuilder.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/AuthRequestUrlBuilder.kt
@@ -7,30 +7,32 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service;
+ */
+package org.mitre.openid.connect.client.service
 
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.config.ServerConfiguration;
-
-import java.util.Map;
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.config.ServerConfiguration
 
 /**
  * Builds a URL string to the IdP's authorization endpoint.
  *
  * @author jricher
- *
  */
-public interface AuthRequestUrlBuilder {
-
-
-	public String buildAuthRequestUrl(ServerConfiguration serverConfig, RegisteredClient clientConfig, String redirectUri, String nonce, String state, Map<String, String> options, String loginHint);
-
+interface AuthRequestUrlBuilder {
+    fun buildAuthRequestUrl(
+        serverConfig: ServerConfiguration?,
+        clientConfig: RegisteredClient?,
+        redirectUri: String?,
+        nonce: String?,
+        state: String?,
+        options: Map<String?, String?>?,
+        loginHint: String?
+    ): String?
 }
