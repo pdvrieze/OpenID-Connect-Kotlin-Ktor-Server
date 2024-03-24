114076d905de199e2a3c66a8b17aad0c4460712d
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-24 19:53:03 +0000

    Convert JpaUtil to Kotlin.

--- openid-connect-common/src/main/java/org/mitre/util/jpa/JpaUtil.kt
+++ openid-connect-common/src/main/java/org/mitre/util/jpa/JpaUtil.kt
@@ -7,62 +7,59 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.util.jpa;
+ */
+package org.mitre.util.jpa
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import org.mitre.data.PageCriteria;
+import org.mitre.data.PageCriteria
+import javax.persistence.EntityManager
+import javax.persistence.TypedQuery
 
 /**
  * @author mfranklin
- *         Date: 4/28/11
- *         Time: 2:13 PM
+ * Date: 4/28/11
+ * Time: 2:13 PM
  */
-public class JpaUtil {
-	public static <T> T getSingleResult(List<T> list) {
-		switch(list.size()) {
-			case 0:
-				return null;
-			case 1:
-				return list.get(0);
-			default:
-				throw new IllegalStateException("Expected single result, got " + list.size());
-		}
-	}
+object JpaUtil {
+    @JvmStatic
+    fun <T> getSingleResult(list: List<T>): T? {
+        return when (list.size) {
+            0 -> null
+            1 -> list[0]
+            else -> throw IllegalStateException("Expected single result, got " + list.size)
+        }
+    }
 
 
-	/**
-	 * Get a page of results from the specified TypedQuery
-	 * by using the given PageCriteria to limit the query
-	 * results. The PageCriteria will override any size or
-	 * offset already specified on the query.
-	 *
-	 * @param <T>  the type parameter
-	 * @param query the query
-	 * @param pageCriteria the page criteria
-	 * @return the list
-	 */
-	public static <T> List<T> getResultPage(TypedQuery<T> query, PageCriteria pageCriteria){
-		query.setMaxResults(pageCriteria.getPageSize());
-		query.setFirstResult(pageCriteria.getPageNumber() * pageCriteria.getPageSize());
+    /**
+     * Get a page of results from the specified TypedQuery
+     * by using the given PageCriteria to limit the query
+     * results. The PageCriteria will override any size or
+     * offset already specified on the query.
+     *
+     * @param T  the type parameter
+     * @param query the query
+     * @param pageCriteria the page criteria
+     * @return the list
+     */
+    @JvmStatic
+    fun <T> getResultPage(query: TypedQuery<T>, pageCriteria: PageCriteria): List<T> {
+        query.setMaxResults(pageCriteria.pageSize)
+        query.setFirstResult(pageCriteria.pageNumber * pageCriteria.pageSize)
 
-		return query.getResultList();
-	}
+        return query.resultList
+    }
 
-	public static <T, I> T saveOrUpdate(I id, EntityManager entityManager, T entity) {
-		T tmp = entityManager.merge(entity);
-		entityManager.flush();
-		return tmp;
-	}
+    @JvmStatic
+    fun <T, I> saveOrUpdate(id: I, entityManager: EntityManager, entity: T): T {
+        val tmp = entityManager.merge(entity)
+        entityManager.flush()
+        return tmp
+    }
 }
