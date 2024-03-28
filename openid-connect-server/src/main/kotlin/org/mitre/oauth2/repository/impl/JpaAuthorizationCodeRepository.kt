76d3d46a51055c44415ff5a6fb2949cf52971d56
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 19:54:32 +0000

    Convert JpaAuthorizationCodeRepository to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/repository/impl/JpaAuthorizationCodeRepository.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/repository/impl/JpaAuthorizationCodeRepository.kt
@@ -7,96 +7,83 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.repository.impl;
+ */
+package org.mitre.oauth2.repository.impl
 
-import org.mitre.data.PageCriteria;
-import org.mitre.oauth2.model.AuthorizationCodeEntity;
-import org.mitre.oauth2.repository.AuthorizationCodeRepository;
-import org.mitre.util.jpa.JpaUtil;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import java.util.Collection;
-import java.util.Date;
+import org.mitre.data.PageCriteria
+import org.mitre.oauth2.model.AuthorizationCodeEntity
+import org.mitre.oauth2.repository.AuthorizationCodeRepository
+import org.mitre.util.jpa.JpaUtil.getResultPage
+import org.mitre.util.jpa.JpaUtil.getSingleResult
+import org.mitre.util.jpa.JpaUtil.saveOrUpdate
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 /**
  * JPA AuthorizationCodeRepository implementation.
  *
  * @author aanganes
- *
  */
 @Repository
-@Transactional(value="defaultTransactionManager")
-public class JpaAuthorizationCodeRepository implements AuthorizationCodeRepository {
+@Transactional(value = "defaultTransactionManager")
+class JpaAuthorizationCodeRepository : AuthorizationCodeRepository {
+    @PersistenceContext(unitName = "defaultPersistenceUnit")
+    lateinit var manager: EntityManager
 
-	@PersistenceContext(unitName="defaultPersistenceUnit")
-	EntityManager manager;
+    @Transactional(value = "defaultTransactionManager")
+    override fun save(authorizationCode: AuthorizationCodeEntity): AuthorizationCodeEntity? {
+        return saveOrUpdate(authorizationCode.id, manager, authorizationCode)
+    }
 
-	/* (non-Javadoc)
-	 * @see org.mitre.oauth2.repository.AuthorizationCodeRepository#save(org.mitre.oauth2.model.AuthorizationCodeEntity)
-	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public AuthorizationCodeEntity save(AuthorizationCodeEntity authorizationCode) {
-
-		return JpaUtil.saveOrUpdate(authorizationCode.getId(), manager, authorizationCode);
-
-	}
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.oauth2.repository.AuthorizationCodeRepository#getByCode(java.lang.String)
 	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public AuthorizationCodeEntity getByCode(String code) {
-		TypedQuery<AuthorizationCodeEntity> query = manager.createNamedQuery(AuthorizationCodeEntity.QUERY_BY_VALUE, AuthorizationCodeEntity.class);
-		query.setParameter("code", code);
+    @Transactional(value = "defaultTransactionManager")
+    override fun getByCode(code: String): AuthorizationCodeEntity? {
+        val query =
+            manager.createNamedQuery(AuthorizationCodeEntity.QUERY_BY_VALUE, AuthorizationCodeEntity::class.java)
+        query.setParameter("code", code)
 
-		AuthorizationCodeEntity result = JpaUtil.getSingleResult(query.getResultList());
-		return result;
-	}
+        val result = getSingleResult(query.resultList)
+        return result
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.oauth2.repository.AuthorizationCodeRepository#remove(org.mitre.oauth2.model.AuthorizationCodeEntity)
 	 */
-	@Override
-	public void remove(AuthorizationCodeEntity authorizationCodeEntity) {
-		AuthorizationCodeEntity found = manager.find(AuthorizationCodeEntity.class, authorizationCodeEntity.getId());
-		if (found != null) {
-			manager.remove(found);
-		}
-	}
+    override fun remove(authorizationCodeEntity: AuthorizationCodeEntity) {
+        val found = manager.find(AuthorizationCodeEntity::class.java, authorizationCodeEntity.id)
+        if (found != null) {
+            manager.remove(found)
+        }
+    }
 
-	/* (non-Javadoc)
+    override val expiredCodes: Collection<AuthorizationCodeEntity>
+        /* (non-Javadoc)
 	 * @see org.mitre.oauth2.repository.AuthorizationCodeRepository#getExpiredCodes()
 	 */
-	@Override
-	public Collection<AuthorizationCodeEntity> getExpiredCodes() {
-		TypedQuery<AuthorizationCodeEntity> query = manager.createNamedQuery(AuthorizationCodeEntity.QUERY_EXPIRATION_BY_DATE, AuthorizationCodeEntity.class);
-		query.setParameter(AuthorizationCodeEntity.PARAM_DATE, new Date()); // this gets anything that's already expired
-		return query.getResultList();
-	}
+        get() {
+            val query =
+                manager.createNamedQuery(AuthorizationCodeEntity.QUERY_EXPIRATION_BY_DATE, AuthorizationCodeEntity::class.java)
+            query.setParameter(AuthorizationCodeEntity.PARAM_DATE, Date()) // this gets anything that's already expired
+            return query.resultList
+        }
 
 
-	@Override
-	public Collection<AuthorizationCodeEntity> getExpiredCodes(PageCriteria pageCriteria) {
-		TypedQuery<AuthorizationCodeEntity> query = manager.createNamedQuery(AuthorizationCodeEntity.QUERY_EXPIRATION_BY_DATE, AuthorizationCodeEntity.class);
-		query.setParameter(AuthorizationCodeEntity.PARAM_DATE, new Date()); // this gets anything that's already expired
-		return JpaUtil.getResultPage(query, pageCriteria);
-	}
-
-
-
+    override fun getExpiredCodes(pageCriteria: PageCriteria): Collection<AuthorizationCodeEntity> {
+        val query =
+            manager.createNamedQuery(AuthorizationCodeEntity.QUERY_EXPIRATION_BY_DATE, AuthorizationCodeEntity::class.java)
+        query.setParameter(AuthorizationCodeEntity.PARAM_DATE, Date()) // this gets anything that's already expired
+        return getResultPage(query, pageCriteria)
+    }
 }
