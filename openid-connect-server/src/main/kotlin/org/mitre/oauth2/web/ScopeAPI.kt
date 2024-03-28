b4ab3a4f97023a44b6ba834c8f03a3e6d5c4976a
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 21:35:29 +0000

    Convert ScopeAPI to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/ScopeAPI.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/ScopeAPI.kt
@@ -7,174 +7,164 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.web;
+ */
+package org.mitre.oauth2.web
 
-import com.google.gson.Gson;
-import org.mitre.oauth2.model.SystemScope;
-import org.mitre.oauth2.service.SystemScopeService;
-import org.mitre.openid.connect.view.HttpCodeView;
-import org.mitre.openid.connect.view.JsonEntityView;
-import org.mitre.openid.connect.view.JsonErrorView;
-import org.mitre.openid.connect.web.RootController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Set;
+import com.google.gson.Gson
+import org.mitre.oauth2.model.SystemScope
+import org.mitre.oauth2.service.SystemScopeService
+import org.mitre.oauth2.util.toJavaId
+import org.mitre.openid.connect.view.HttpCodeView
+import org.mitre.openid.connect.view.JsonEntityView
+import org.mitre.openid.connect.view.JsonErrorView
+import org.mitre.openid.connect.web.RootController
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 /**
  * @author jricher
- *
  */
 @Controller
 @RequestMapping("/" + ScopeAPI.URL)
 @PreAuthorize("hasRole('ROLE_USER')")
-public class ScopeAPI {
+class ScopeAPI {
+    @Autowired
+    private lateinit var scopeService: SystemScopeService
 
-	public static final String URL = RootController.API_URL + "/scopes";
+    private val gson = Gson()
 
-	@Autowired
-	private SystemScopeService scopeService;
+    @RequestMapping(value = [""], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getAll(m: ModelMap): String {
+        val allScopes = scopeService.all
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(ScopeAPI.class);
+        m[JsonEntityView.ENTITY] = allScopes
 
-	private Gson gson = new Gson();
+        return JsonEntityView.VIEWNAME
+    }
 
-	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getAll(ModelMap m) {
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getScope(@PathVariable("id") id: Long, m: ModelMap): String {
+        val scope = scopeService.getById(id.toJavaId())
 
-		Set<SystemScope> allScopes = scopeService.getAll();
+        if (scope != null) {
+            m[JsonEntityView.ENTITY] = scope
 
-		m.put(JsonEntityView.ENTITY, allScopes);
+            return JsonEntityView.VIEWNAME
+        } else {
+            logger.error("getScope failed; scope not found: $id")
 
-		return JsonEntityView.VIEWNAME;
-	}
+            m[HttpCodeView.CODE] = HttpStatus.NOT_FOUND
+            m[JsonErrorView.ERROR_MESSAGE] = "The requested scope with id $id could not be found."
+            return JsonErrorView.VIEWNAME
+        }
+    }
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getScope(@PathVariable("id") Long id, ModelMap m) {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.PUT], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun updateScope(@PathVariable("id") id: Long, @RequestBody json: String?, m: ModelMap): String {
+        val existing = scopeService.getById(id.toJavaId())
 
-		SystemScope scope = scopeService.getById(id);
+        var scope = gson.fromJson(json, SystemScope::class.java)
 
-		if (scope != null) {
+        if (existing != null && scope != null) {
+            if (existing.id == scope.id) {
+                // sanity check
 
-			m.put(JsonEntityView.ENTITY, scope);
+                scope = scopeService.save(scope)!!
 
-			return JsonEntityView.VIEWNAME;
-		} else {
+                m[JsonEntityView.ENTITY] = scope
 
-			logger.error("getScope failed; scope not found: " + id);
+                return JsonEntityView.VIEWNAME
+            } else {
+                logger.error(
+                    "updateScope failed; scope ids to not match: got "
+                            + existing.id + " and " + scope.id
+                )
 
-			m.put(HttpCodeView.CODE, HttpStatus.NOT_FOUND);
-			m.put(JsonErrorView.ERROR_MESSAGE, "The requested scope with id " + id + " could not be found.");
-			return JsonErrorView.VIEWNAME;
-		}
-	}
+                m[HttpCodeView.CODE] = HttpStatus.BAD_REQUEST
+                m[JsonErrorView.ERROR_MESSAGE] = ("Could not update scope. Scope ids to not match: got "
+                        + existing.id + " and " + scope.id)
+                return JsonErrorView.VIEWNAME
+            }
+        } else {
+            logger.error("updateScope failed; scope with id $id not found.")
+            m[HttpCodeView.CODE] = HttpStatus.NOT_FOUND
+            m[JsonErrorView.ERROR_MESSAGE] = "Could not update scope. The scope with id $id could not be found."
+            return JsonErrorView.VIEWNAME
+        }
+    }
 
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public String updateScope(@PathVariable("id") Long id, @RequestBody String json, ModelMap m) {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = [""], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun createScope(@RequestBody json: String, m: ModelMap): String {
+        var scope = gson.fromJson(json, SystemScope::class.java)
 
-		SystemScope existing = scopeService.getById(id);
+        val alreadyExists = scopeService.getByValue(scope!!.value!!)
+        if (alreadyExists != null) {
+            //Error, cannot save a scope with the same value as an existing one
+            logger.error("Error: attempting to save a scope with a value that already exists: " + scope.value)
+            m[HttpCodeView.CODE] = HttpStatus.CONFLICT
+            m[JsonErrorView.ERROR_MESSAGE] =
+                "A scope with value " + scope.value + " already exists, please choose a different value."
+            return JsonErrorView.VIEWNAME
+        }
 
-		SystemScope scope = gson.fromJson(json, SystemScope.class);
+        scope = scopeService.save(scope)
 
-		if (existing != null && scope != null) {
+        if (scope?.id != null) {
+            m[JsonEntityView.ENTITY] = scope
 
-			if (existing.getId().equals(scope.getId())) {
-				// sanity check
+            return JsonEntityView.VIEWNAME
+        } else {
+            logger.error("createScope failed; JSON was invalid: $json")
+            m[HttpCodeView.CODE] = HttpStatus.BAD_REQUEST
+            m[JsonErrorView.ERROR_MESSAGE] =
+                "Could not save new scope $scope. The scope service failed to return a saved entity."
+            return JsonErrorView.VIEWNAME
+        }
+    }
 
-				scope = scopeService.save(scope);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE])
+    fun deleteScope(@PathVariable("id") id: Long, m: ModelMap): String {
+        val existing = scopeService.getById(id.toJavaId())
 
-				m.put(JsonEntityView.ENTITY, scope);
+        if (existing != null) {
+            scopeService.remove(existing)
 
-				return JsonEntityView.VIEWNAME;
-			} else {
+            return HttpCodeView.VIEWNAME
+        } else {
+            logger.error("deleteScope failed; scope with id $id not found.")
+            m[HttpCodeView.CODE] = HttpStatus.NOT_FOUND
+            m[JsonErrorView.ERROR_MESSAGE] =
+                "Could not delete scope. The requested scope with id $id could not be found."
+            return JsonErrorView.VIEWNAME
+        }
+    }
 
-				logger.error("updateScope failed; scope ids to not match: got "
-						+ existing.getId() + " and " + scope.getId());
+    companion object {
+        const val URL: String = RootController.API_URL + "/scopes"
 
-				m.put(HttpCodeView.CODE, HttpStatus.BAD_REQUEST);
-				m.put(JsonErrorView.ERROR_MESSAGE, "Could not update scope. Scope ids to not match: got "
-						+ existing.getId() + " and " + scope.getId());
-				return JsonErrorView.VIEWNAME;
-			}
-
-		} else {
-
-			logger.error("updateScope failed; scope with id " + id + " not found.");
-			m.put(HttpCodeView.CODE, HttpStatus.NOT_FOUND);
-			m.put(JsonErrorView.ERROR_MESSAGE, "Could not update scope. The scope with id " + id + " could not be found.");
-			return JsonErrorView.VIEWNAME;
-		}
-	}
-
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public String createScope(@RequestBody String json, ModelMap m) {
-		SystemScope scope = gson.fromJson(json, SystemScope.class);
-
-		SystemScope alreadyExists = scopeService.getByValue(scope.getValue());
-		if (alreadyExists != null) {
-			//Error, cannot save a scope with the same value as an existing one
-			logger.error("Error: attempting to save a scope with a value that already exists: " + scope.getValue());
-			m.put(HttpCodeView.CODE, HttpStatus.CONFLICT);
-			m.put(JsonErrorView.ERROR_MESSAGE, "A scope with value " + scope.getValue() + " already exists, please choose a different value.");
-			return JsonErrorView.VIEWNAME;
-		}
-
-		scope = scopeService.save(scope);
-
-		if (scope != null && scope.getId() != null) {
-
-			m.put(JsonEntityView.ENTITY, scope);
-
-			return JsonEntityView.VIEWNAME;
-		} else {
-
-			logger.error("createScope failed; JSON was invalid: " + json);
-			m.put(HttpCodeView.CODE, HttpStatus.BAD_REQUEST);
-			m.put(JsonErrorView.ERROR_MESSAGE, "Could not save new scope " + scope + ". The scope service failed to return a saved entity.");
-			return JsonErrorView.VIEWNAME;
-
-		}
-	}
-
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public String deleteScope(@PathVariable("id") Long id, ModelMap m) {
-		SystemScope existing = scopeService.getById(id);
-
-		if (existing != null) {
-
-			scopeService.remove(existing);
-
-			return HttpCodeView.VIEWNAME;
-		} else {
-
-			logger.error("deleteScope failed; scope with id " + id + " not found.");
-			m.put(HttpCodeView.CODE, HttpStatus.NOT_FOUND);
-			m.put(JsonErrorView.ERROR_MESSAGE, "Could not delete scope. The requested scope with id " + id + " could not be found.");
-			return JsonErrorView.VIEWNAME;
-		}
-	}
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(ScopeAPI::class.java)
+    }
 }
