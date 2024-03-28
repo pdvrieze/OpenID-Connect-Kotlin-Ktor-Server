81690f8c587359987b7c9039810e0f763acd79a7
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 16:14:05 +0000

    Convert JsonFileRegisteredClientService to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/JsonFileRegisteredClientService.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/JsonFileRegisteredClientService.kt
@@ -7,133 +7,105 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service.impl;
+ */
+package org.mitre.openid.connect.client.service.impl
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.ClientDetailsEntityJsonProcessor;
-import org.mitre.openid.connect.client.service.RegisteredClientService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonSerializer
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.ClientDetailsEntityJsonProcessor.parseRegistered
+import org.mitre.openid.connect.ClientDetailsEntityJsonProcessor.serialize
+import org.mitre.openid.connect.client.service.RegisteredClientService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import java.io.IOException
 
 /**
  * @author jricher
- *
  */
-public class JsonFileRegisteredClientService implements RegisteredClientService {
+class JsonFileRegisteredClientService(filename: String) : RegisteredClientService {
+    private val gson: Gson = GsonBuilder()
+        .registerTypeAdapter(RegisteredClient::class.java, JsonSerializer<RegisteredClient> { src, typeOfSrc, context -> serialize(src)!! })
+        .registerTypeAdapter(RegisteredClient::class.java, JsonDeserializer { json, typeOfT, context -> parseRegistered(json) })
+        .setPrettyPrinting()
+        .create()
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(JsonFileRegisteredClientService.class);
+    private val file = File(filename)
 
-	private Gson gson = new GsonBuilder()
-			.registerTypeAdapter(RegisteredClient.class, new JsonSerializer<RegisteredClient>() {
-				@Override
-				public JsonElement serialize(RegisteredClient src, Type typeOfSrc, JsonSerializationContext context) {
-					return ClientDetailsEntityJsonProcessor.serialize(src);
-				}
-			})
-			.registerTypeAdapter(RegisteredClient.class, new JsonDeserializer<RegisteredClient>() {
-				@Override
-				public RegisteredClient deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-					return ClientDetailsEntityJsonProcessor.parseRegistered(json);
-				}
-			})
-			.setPrettyPrinting()
-			.create();
+    private var clients: MutableMap<String, RegisteredClient> = HashMap()
 
-	private File file;
+    init {
+        load()
+    }
 
-	private Map<String, RegisteredClient> clients = new HashMap<>();
-
-	public JsonFileRegisteredClientService(String filename) {
-		this.file = new File(filename);
-		load();
-	}
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.client.service.RegisteredClientService#getByIssuer(java.lang.String)
 	 */
-	@Override
-	public RegisteredClient getByIssuer(String issuer) {
-		return clients.get(issuer);
-	}
+    override fun getByIssuer(issuer: String): RegisteredClient? {
+        return clients[issuer]
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.client.service.RegisteredClientService#save(java.lang.String, org.mitre.oauth2.model.RegisteredClient)
 	 */
-	@Override
-	public void save(String issuer, RegisteredClient client) {
-		clients.put(issuer, client);
-		write();
-	}
+    override fun save(issuer: String, client: RegisteredClient) {
+        clients[issuer] = client
+        write()
+    }
 
-	/**
-	 * Sync the map of clients out to disk.
-	 */
-	@SuppressWarnings("serial")
-	private void write() {
-		try {
-			if (!file.exists()) {
-				// create a new file
-				logger.info("Creating saved clients list in " + file);
-				file.createNewFile();
-			}
-			FileWriter out = new FileWriter(file);
+    /**
+     * Sync the map of clients out to disk.
+     */
+    private fun write() {
+        try {
+            if (!file.exists()) {
+                // create a new file
+                logger.info("Creating saved clients list in $file")
+                file.createNewFile()
+            }
 
-			gson.toJson(clients, new TypeToken<Map<String, RegisteredClient>>(){}.getType(), out);
+            FileWriter(file).use { out ->
+                gson.toJson(clients, object : TypeToken<Map<String?, RegisteredClient?>?>() {}.type, out)
+            }
+        } catch (e: IOException) {
+            logger.error("Could not write to output file", e)
+        }
+    }
 
-			out.close();
+    /**
+     * Load the map in from disk.
+     */
+    private fun load() {
+        try {
+            if (!file.exists()) {
+                logger.info("No sved clients file found in $file")
+                return
+            }
+            FileReader(file).use { reader ->
+                clients = gson.fromJson(reader, object : TypeToken<Map<String?, RegisteredClient?>?>() {}.type)
+            }
+        } catch (e: IOException) {
+            logger.error("Could not read from input file", e)
+        }
+    }
 
-		} catch (IOException e) {
-			logger.error("Could not write to output file", e);
-		}
-	}
-
-	/**
-	 * Load the map in from disk.
-	 */
-	@SuppressWarnings("serial")
-	private void load() {
-		try {
-			if (!file.exists()) {
-				logger.info("No sved clients file found in " + file);
-				return;
-			}
-			FileReader in = new FileReader(file);
-
-			clients = gson.fromJson(in, new TypeToken<Map<String, RegisteredClient>>(){}.getType());
-
-			in.close();
-
-		} catch (IOException e) {
-			logger.error("Could not read from input file", e);
-		}
-	}
-
+    companion object {
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(JsonFileRegisteredClientService::class.java)
+    }
 }
