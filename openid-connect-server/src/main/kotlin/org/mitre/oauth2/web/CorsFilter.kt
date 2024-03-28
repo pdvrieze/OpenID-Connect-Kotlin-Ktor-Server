ec6ac8a2f5e1b3316226018e9a1a9a404a278573
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 21:49:19 +0000

    Convert CorsFilter to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/CorsFilter.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/CorsFilter.kt
@@ -7,25 +7,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.web;
+ */
+package org.mitre.oauth2.web
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  *
@@ -34,24 +32,21 @@ import java.io.IOException;
  * on endpoints that require CORS support.
  *
  * @author jricher
- *
  */
 @Component("corsFilter")
-public class CorsFilter extends OncePerRequestFilter {
-
-	/* (non-Javadoc)
-	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
-	 */
-	@Override
-	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-			// CORS "pre-flight" request
-			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-			response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept, Authorization");
-		}
-		filterChain.doFilter(request, response);
-	}
-
+class CorsFilter : OncePerRequestFilter() {
+    @Throws(IOException::class, ServletException::class)
+    public override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        response.addHeader("Access-Control-Allow-Origin", "*")
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS" == request.method) {
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+            response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept, Authorization")
+        }
+        filterChain.doFilter(request, response)
+    }
 }
