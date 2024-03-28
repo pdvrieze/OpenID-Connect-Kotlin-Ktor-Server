ebc984dfe079b197ddb2b166bda79f280fbe4c17
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 22:35:07 +0000

    Convert DataAPI to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/DataAPI.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/DataAPI.kt
@@ -7,150 +7,151 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.web;
+ */
+package org.mitre.openid.connect.web
 
-import java.io.IOException;
-import java.io.Reader;
-import java.security.Principal;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.mitre.openid.connect.config.ConfigurationPropertiesBean;
-import org.mitre.openid.connect.service.MITREidDataService;
-import org.mitre.openid.connect.service.impl.MITREidDataService_1_3;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.google.common.collect.ImmutableList;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
+import com.google.common.collect.ImmutableList
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
+import com.google.gson.stream.JsonWriter
+import org.mitre.openid.connect.config.ConfigurationPropertiesBean
+import org.mitre.openid.connect.service.MITREidDataService
+import org.mitre.openid.connect.service.impl.MITREidDataService_1_3
+import org.mitre.openid.connect.web.DataAPI
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import java.io.IOException
+import java.io.Reader
+import java.security.Principal
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 /**
  * API endpoint for importing and exporting the current state of a server.
  * Includes all tokens, grants, whitelists, blacklists, and clients.
  *
  * @author jricher
- *
  */
 @Controller
 @RequestMapping("/" + DataAPI.URL)
 @PreAuthorize("hasRole('ROLE_ADMIN')") // you need to be an admin to even think about this -- this is a potentially dangerous API!!
-public class DataAPI {
+class DataAPI {
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
 
-	public static final String URL = RootController.API_URL + "/data";
+    @Autowired
+    private lateinit var config: ConfigurationPropertiesBean
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(DataAPI.class);
+    @Autowired
+    private lateinit var importers: List<MITREidDataService>
 
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+    private val supportedVersions: List<String> = ImmutableList.of(
+        MITREidDataService.MITREID_CONNECT_1_0,
+        MITREidDataService.MITREID_CONNECT_1_1,
+        MITREidDataService.MITREID_CONNECT_1_2,
+        MITREidDataService.MITREID_CONNECT_1_3
+    )
 
-	@Autowired
-	private ConfigurationPropertiesBean config;
+    @Autowired
+    private lateinit var exporter: MITREidDataService_1_3
 
-	@Autowired
-	private List<MITREidDataService> importers;
+    @RequestMapping(method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @Throws(IOException::class)
+    fun importData(`in`: Reader?, m: Model?): String {
+        val reader = JsonReader(`in`)
 
-	private List<String> supportedVersions = ImmutableList.of(
-			MITREidDataService.MITREID_CONNECT_1_0,
-			MITREidDataService.MITREID_CONNECT_1_1,
-			MITREidDataService.MITREID_CONNECT_1_2,
-			MITREidDataService.MITREID_CONNECT_1_3);
+        reader.beginObject()
 
-	@Autowired
-	private MITREidDataService_1_3 exporter;
+        while (reader.hasNext()) {
+            val tok = reader.peek()
+            when (tok) {
+                JsonToken.NAME -> {
+                    val name = reader.nextName()
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public String importData(Reader in, Model m) throws IOException {
+                    if (supportedVersions.contains(name)) {
+                        // we're working with a known data version tag
+                        for (dataService in importers) {
+                            // dispatch to the correct service
+                            if (dataService.supportsVersion(name)) {
+                                dataService.importData(reader)
+                                break
+                            }
+                        }
+                    } else {
+                        // consume the next bit silently for now
+                        logger.debug("Skipping value for $name") // TODO: write these out?
+                        reader.skipValue()
+                    }
+                }
 
-		JsonReader reader = new JsonReader(in);
+                JsonToken.END_OBJECT -> {}
+                JsonToken.END_DOCUMENT -> {}
+                JsonToken.BEGIN_ARRAY -> TODO()
+                JsonToken.END_ARRAY -> TODO()
+                JsonToken.BEGIN_OBJECT -> TODO()
+                JsonToken.STRING -> TODO()
+                JsonToken.NUMBER -> TODO()
+                JsonToken.BOOLEAN -> TODO()
+                JsonToken.NULL -> TODO()
+            }
+        }
 
-		reader.beginObject();
+        reader.endObject()
 
-		while (reader.hasNext()) {
-			JsonToken tok = reader.peek();
-			switch (tok) {
-				case NAME:
-					String name = reader.nextName();
+        return "httpCodeView"
+    }
 
-					if (supportedVersions.contains(name)) {
-						// we're working with a known data version tag
-						for (MITREidDataService dataService : importers) {
-							// dispatch to the correct service
-							if (dataService.supportsVersion(name)) {
-								dataService.importData(reader);
-								break;
-							}
-						}
-					} else {
-						// consume the next bit silently for now
-						logger.debug("Skipping value for " + name); // TODO: write these out?
-						reader.skipValue();
-					}
-					break;
-				case END_OBJECT:
-					break;
-				case END_DOCUMENT:
-					break;
-			}
-		}
+    @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Throws(IOException::class)
+    fun exportData(resp: HttpServletResponse, prin: Principal) {
+        resp.contentType = MediaType.APPLICATION_JSON_VALUE
 
-		reader.endObject();
+        // this writer puts things out onto the wire
+        val writer = JsonWriter(resp.writer)
+        writer.setIndent("  ")
 
-		return "httpCodeView";
-	}
+        try {
+            writer.beginObject()
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void exportData(HttpServletResponse resp, Principal prin) throws IOException {
+            writer.name("exported-at")
+            writer.value(dateFormat.format(Date()))
 
-		resp.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            writer.name("exported-from")
+            writer.value(config.issuer)
 
-		// this writer puts things out onto the wire
-		JsonWriter writer = new JsonWriter(resp.getWriter());
-		writer.setIndent("  ");
+            writer.name("exported-by")
+            writer.value(prin.name)
 
-		try {
+            // delegate to the service to do the actual export
+            exporter.exportData(writer)
 
-			writer.beginObject();
+            writer.endObject() // end root
+            writer.close()
+        } catch (e: IOException) {
+            logger.error("Unable to export data", e)
+        }
+    }
 
-			writer.name("exported-at");
-			writer.value(dateFormat.format(new Date()));
+    companion object {
+        const val URL: String = RootController.API_URL + "/data"
 
-			writer.name("exported-from");
-			writer.value(config.getIssuer());
-
-			writer.name("exported-by");
-			writer.value(prin.getName());
-
-			// delegate to the service to do the actual export
-			exporter.exportData(writer);
-
-			writer.endObject(); // end root
-			writer.close();
-
-		} catch (IOException e) {
-			logger.error("Unable to export data", e);
-		}
-	}
-
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(DataAPI::class.java)
+    }
 }
