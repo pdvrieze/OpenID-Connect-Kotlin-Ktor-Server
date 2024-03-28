1c032031ae2ca282374b30a4a411525c1ff769cb
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 21:53:19 +0000

    Convert AuthorizationPendingException, DeviceCodeExpiredException and DuplicateClientIdException to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/exception/DuplicateClientIdException.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/exception/DuplicateClientIdException.kt
@@ -7,24 +7,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.exception;
+ */
+package org.mitre.oauth2.exception
 
-public class DuplicateClientIdException extends RuntimeException {
-
-	public DuplicateClientIdException(String clientId) {
-		super("Duplicate client id: " + clientId);
-	}
-
-
-	private static final long serialVersionUID = 1L;
-
-
+class DuplicateClientIdException(clientId: String) : RuntimeException("Duplicate client id: $clientId") {
+    companion object {
+        private const val serialVersionUID = 1L
+    }
 }
--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/view/TokenApiView.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/view/TokenApiView.kt
@@ -5,146 +5,110 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.view;
+ */
+package org.mitre.oauth2.view
 
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.reflect.Type;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
-import org.mitre.oauth2.model.OAuth2RefreshTokenEntity;
-import org.mitre.openid.connect.view.HttpCodeView;
-import org.mitre.openid.connect.view.JsonEntityView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.web.servlet.view.AbstractView;
-
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.JsonSerializer
+import org.mitre.oauth2.model.OAuth2AccessTokenEntity
+import org.mitre.oauth2.model.OAuth2RefreshTokenEntity
+import org.mitre.oauth2.view.TokenApiView
+import org.mitre.openid.connect.view.HttpCodeView
+import org.mitre.openid.connect.view.JsonEntityView
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.web.servlet.view.AbstractView
+import java.io.IOException
+import java.io.Writer
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Component(TokenApiView.VIEWNAME)
-public class TokenApiView extends AbstractView {
+class TokenApiView : AbstractView() {
+    private val gson: Gson = GsonBuilder()
+        .setExclusionStrategies(object : ExclusionStrategy {
+            override fun shouldSkipField(f: FieldAttributes): Boolean {
+                return false
+            }
 
-	public static final String VIEWNAME = "tokenApiView";
+            override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                // skip the JPA binding wrapper
+                return clazz == BeanPropertyBindingResult::class.java
+            }
+        })
+        .registerTypeAdapter(OAuth2AccessTokenEntity::class.java, JsonSerializer<OAuth2AccessTokenEntity> { src, typeOfSrc, context ->
+            val o = JsonObject()
+            o.addProperty("value", src.value)
+            o.addProperty("id", src.id)
+            o.addProperty("refreshTokenId", if (src.refreshToken != null) src.refreshToken!!.id else null)
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(TokenApiView.class);
+            o.add("scopes", context.serialize(src.scope))
 
-	private Gson gson = new GsonBuilder()
-			.setExclusionStrategies(new ExclusionStrategy() {
+            o.addProperty("clientId", src.client!!.clientId)
+            o.addProperty("userId", src.authenticationHolder.authentication.name)
 
-				@Override
-				public boolean shouldSkipField(FieldAttributes f) {
-					return false;
-				}
+            o.add("expiration", context.serialize(src.expiration))
+            o
+        })
+        .registerTypeAdapter(OAuth2RefreshTokenEntity::class.java, JsonSerializer<OAuth2RefreshTokenEntity> { src, typeOfSrc, context ->
+            val o = JsonObject()
+            o.addProperty("value", src.value)
+            o.addProperty("id", src.id)
 
-				@Override
-				public boolean shouldSkipClass(Class<?> clazz) {
-					// skip the JPA binding wrapper
-					if (clazz.equals(BeanPropertyBindingResult.class)) {
-						return true;
-					}
-					return false;
-				}
+            o.add("scopes", context.serialize(src.authenticationHolder.authentication.oAuth2Request.scope))
 
-			})
-			.registerTypeAdapter(OAuth2AccessTokenEntity.class, new JsonSerializer<OAuth2AccessTokenEntity>() {
+            o.addProperty("clientId", src.client!!.clientId)
+            o.addProperty("userId", src.authenticationHolder.authentication.name)
 
-				@Override
-				public JsonElement serialize(OAuth2AccessTokenEntity src,
-						Type typeOfSrc, JsonSerializationContext context) {
+            o.add("expiration", context.serialize(src.expiration))
+            o
+        })
+        .serializeNulls()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+        .create()
 
+    override fun renderMergedOutputModel(
+        model: Map<String, Any>,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
 
-					JsonObject o = new JsonObject();
+        // default to 200
+        val code = model[HttpCodeView.CODE] as HttpStatus? ?: HttpStatus.OK
 
-					o.addProperty("value", src.getValue());
-					o.addProperty("id", src.getId());
-					o.addProperty("refreshTokenId", src.getRefreshToken() != null ? src.getRefreshToken().getId() : null);
+        response.setStatus(code.value())
 
-					o.add("scopes", context.serialize(src.getScope()));
+        try {
+            val out: Writer = response.writer
+            val obj = model[JsonEntityView.ENTITY]
+            gson.toJson(obj, out)
+        } catch (e: IOException) {
+            Companion.logger.error("IOException in JsonEntityView.java: ", e)
+        }
+    }
 
-					o.addProperty("clientId", src.getClient().getClientId());
-					o.addProperty("userId", src.getAuthenticationHolder().getAuthentication().getName());
+    companion object {
+        const val VIEWNAME: String = "tokenApiView"
 
-					o.add("expiration", context.serialize(src.getExpiration()));
-
-					return o;
-				}
-
-			})
-			.registerTypeAdapter(OAuth2RefreshTokenEntity.class, new JsonSerializer<OAuth2RefreshTokenEntity>() {
-
-				@Override
-				public JsonElement serialize(OAuth2RefreshTokenEntity src,
-						Type typeOfSrc, JsonSerializationContext context) {
-					JsonObject o = new JsonObject();
-
-					o.addProperty("value", src.getValue());
-					o.addProperty("id", src.getId());
-
-					o.add("scopes", context.serialize(src.getAuthenticationHolder().getAuthentication().getOAuth2Request().getScope()));
-
-					o.addProperty("clientId", src.getClient().getClientId());
-					o.addProperty("userId", src.getAuthenticationHolder().getAuthentication().getName());
-
-					o.add("expiration", context.serialize(src.getExpiration()));
-
-					return o;
-				}
-
-			})
-			.serializeNulls()
-			.setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-			.create();
-
-	@Override
-	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) {
-
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-
-		HttpStatus code = (HttpStatus) model.get(HttpCodeView.CODE);
-		if (code == null) {
-			code = HttpStatus.OK; // default to 200
-		}
-
-		response.setStatus(code.value());
-
-		try {
-
-			Writer out = response.getWriter();
-			Object obj = model.get(JsonEntityView.ENTITY);
-			gson.toJson(obj, out);
-
-		} catch (IOException e) {
-
-			logger.error("IOException in JsonEntityView.java: ", e);
-
-		}
-	}
-
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(TokenApiView::class.java)
+    }
 }
