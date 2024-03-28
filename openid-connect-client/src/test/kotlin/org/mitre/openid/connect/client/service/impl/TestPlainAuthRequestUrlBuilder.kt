82f19fff2e0f72edde667504db236e095624d710
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 17:52:56 +0000

    Convert TestPlainAuthRequestUrlBuilder to Kotlin.

--- openid-connect-client/src/test/kotlin/org/mitre/openid/connect/client/service/impl/TestPlainAuthRequestUrlBuilder.kt
+++ openid-connect-client/src/test/kotlin/org/mitre/openid/connect/client/service/impl/TestPlainAuthRequestUrlBuilder.kt
@@ -7,102 +7,95 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service.impl;
+ */
+package org.mitre.openid.connect.client.service.impl
 
-import java.util.Map;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.config.ServerConfiguration;
-import org.mockito.Mockito;
-import org.springframework.security.authentication.AuthenticationServiceException;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Sets;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-
-import static org.junit.Assert.assertThat;
+import com.google.common.collect.ImmutableMap
+import com.google.common.collect.Sets
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.config.ServerConfiguration
+import org.mockito.Mockito
+import org.springframework.security.authentication.AuthenticationServiceException
 
 /**
  * @author wkim
- *
  */
-public class TestPlainAuthRequestUrlBuilder {
+class TestPlainAuthRequestUrlBuilder {
+    // Test fixture:
+    lateinit var serverConfig: ServerConfiguration
+    lateinit var clientConfig: RegisteredClient
 
-	// Test fixture:
-	ServerConfiguration serverConfig;
-	RegisteredClient clientConfig;
+    private val urlBuilder = PlainAuthRequestUrlBuilder()
 
-	private PlainAuthRequestUrlBuilder urlBuilder = new PlainAuthRequestUrlBuilder();
+    @BeforeEach
+    fun prepare() {
+        serverConfig = Mockito.mock(ServerConfiguration::class.java)
+        Mockito.`when`(serverConfig.authorizationEndpointUri).thenReturn("https://server.example.com/authorize")
 
-	@Before
-	public void prepare() {
+        clientConfig = Mockito.mock(RegisteredClient::class.java)
+        Mockito.`when`(clientConfig.clientId).thenReturn("s6BhdRkqt3")
+        Mockito.`when`(clientConfig.scope).thenReturn(Sets.newHashSet("openid", "profile"))
+    }
 
-		serverConfig = Mockito.mock(ServerConfiguration.class);
-		Mockito.when(serverConfig.getAuthorizationEndpointUri()).thenReturn("https://server.example.com/authorize");
+    @Test
+    fun buildAuthRequestUrl() {
+        val expectedUrl = "https://server.example.com/authorize?" +
+                "response_type=code" +
+                "&client_id=s6BhdRkqt3" +
+                "&scope=openid+profile" +  // plus sign used for space per application/x-www-form-encoded standard
+                "&redirect_uri=https%3A%2F%2Fclient.example.org%2F" +
+                "&nonce=34fasf3ds" +
+                "&state=af0ifjsldkj" +
+                "&foo=bar"
 
-		clientConfig = Mockito.mock(RegisteredClient.class);
-		Mockito.when(clientConfig.getClientId()).thenReturn("s6BhdRkqt3");
-		Mockito.when(clientConfig.getScope()).thenReturn(Sets.newHashSet("openid", "profile"));
-	}
+        val options: Map<String, String> = ImmutableMap.of("foo", "bar")
 
-	@Test
-	public void buildAuthRequestUrl() {
+        val actualUrl =
+            urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "https://client.example.org/", "34fasf3ds", "af0ifjsldkj", options, null)
 
-		String expectedUrl = "https://server.example.com/authorize?" +
-				"response_type=code" +
-				"&client_id=s6BhdRkqt3" +
-				"&scope=openid+profile" + // plus sign used for space per application/x-www-form-encoded standard
-				"&redirect_uri=https%3A%2F%2Fclient.example.org%2F" +
-				"&nonce=34fasf3ds" +
-				"&state=af0ifjsldkj" +
-				"&foo=bar";
+        assertThat(actualUrl, CoreMatchers.equalTo(expectedUrl))
+    }
 
-		Map<String, String> options = ImmutableMap.of("foo", "bar");
+    @Test
+    fun buildAuthRequestUrl_withLoginHint() {
+        val expectedUrl = "https://server.example.com/authorize?" +
+                "response_type=code" +
+                "&client_id=s6BhdRkqt3" +
+                "&scope=openid+profile" +  // plus sign used for space per application/x-www-form-encoded standard
+                "&redirect_uri=https%3A%2F%2Fclient.example.org%2F" +
+                "&nonce=34fasf3ds" +
+                "&state=af0ifjsldkj" +
+                "&foo=bar" +
+                "&login_hint=bob"
 
-		String actualUrl = urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "https://client.example.org/", "34fasf3ds", "af0ifjsldkj", options, null);
+        val options: Map<String, String> = ImmutableMap.of("foo", "bar")
 
-		assertThat(actualUrl, equalTo(expectedUrl));
-	}
+        val actualUrl =
+            urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "https://client.example.org/", "34fasf3ds", "af0ifjsldkj", options, "bob")
 
-	@Test
-	public void buildAuthRequestUrl_withLoginHint() {
+        assertThat(actualUrl, CoreMatchers.equalTo(expectedUrl))
+    }
 
-		String expectedUrl = "https://server.example.com/authorize?" +
-				"response_type=code" +
-				"&client_id=s6BhdRkqt3" +
-				"&scope=openid+profile" + // plus sign used for space per application/x-www-form-encoded standard
-				"&redirect_uri=https%3A%2F%2Fclient.example.org%2F" +
-				"&nonce=34fasf3ds" +
-				"&state=af0ifjsldkj" +
-				"&foo=bar" +
-				"&login_hint=bob";
+    @Test
+    fun buildAuthRequestUrl_badUri() {
+        Mockito.`when`(serverConfig.authorizationEndpointUri).thenReturn("e=mc^2")
 
-		Map<String, String> options = ImmutableMap.of("foo", "bar");
+        val options: Map<String, String> = ImmutableMap.of("foo", "bar")
 
-		String actualUrl = urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "https://client.example.org/", "34fasf3ds", "af0ifjsldkj", options, "bob");
-
-		assertThat(actualUrl, equalTo(expectedUrl));
-	}
-
-	@Test(expected = AuthenticationServiceException.class)
-	public void buildAuthRequestUrl_badUri() {
-
-		Mockito.when(serverConfig.getAuthorizationEndpointUri()).thenReturn("e=mc^2");
-
-		Map<String, String> options = ImmutableMap.of("foo", "bar");
-
-		urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "example.com", "", "", options, null);
-	}
-
+        assertThrows<AuthenticationServiceException> {
+            urlBuilder.buildAuthRequestUrl(serverConfig, clientConfig, "example.com", "", "", options, null)
+        }
+    }
 }
