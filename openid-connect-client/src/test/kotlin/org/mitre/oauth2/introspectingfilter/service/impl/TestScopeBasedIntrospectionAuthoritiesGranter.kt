407bf03e162805f20e5227a109428a93c1279e3b
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 16:38:06 +0000

    Convert TestScopeBasedIntrospectionAuthoritiesGranter to Kotlin

---
+++ openid-connect-client/src/test/kotlin/org/mitre/oauth2/introspectingfilter/service/impl/TestScopeBasedIntrospectionAuthoritiesGranter.kt
@@ -0,0 +1,60 @@
+package org.mitre.oauth2.introspectingfilter.service.impl
+
+import com.google.gson.JsonObject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+/**
+ * @author jricher
+ */
+class TestScopeBasedIntrospectionAuthoritiesGranter {
+    private lateinit var introspectionResponse: JsonObject
+
+    private val granter = ScopeBasedIntrospectionAuthoritiesGranter()
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    @Throws(Exception::class)
+    fun setUp() {
+        introspectionResponse = JsonObject()
+    }
+
+    /**
+     * Test method for [org.mitre.oauth2.introspectingfilter.service.impl.ScopeBasedIntrospectionAuthoritiesGranter.getAuthorities].
+     */
+    @Test
+    fun testGetAuthoritiesJsonObject_withScopes() {
+        introspectionResponse.addProperty("scope", "foo bar baz batman")
+
+        val expected: MutableList<GrantedAuthority> = ArrayList()
+        expected.add(SimpleGrantedAuthority("ROLE_API"))
+        expected.add(SimpleGrantedAuthority("OAUTH_SCOPE_foo"))
+        expected.add(SimpleGrantedAuthority("OAUTH_SCOPE_bar"))
+        expected.add(SimpleGrantedAuthority("OAUTH_SCOPE_baz"))
+        expected.add(SimpleGrantedAuthority("OAUTH_SCOPE_batman"))
+
+        val authorities = granter.getAuthorities(introspectionResponse)
+
+        Assertions.assertTrue(authorities.containsAll(expected))
+        Assertions.assertTrue(expected.containsAll(authorities))
+    }
+
+    /**
+     * Test method for [org.mitre.oauth2.introspectingfilter.service.impl.ScopeBasedIntrospectionAuthoritiesGranter.getAuthorities].
+     */
+    @Test
+    fun testGetAuthoritiesJsonObject_withoutScopes() {
+        val expected: MutableList<GrantedAuthority> = ArrayList()
+        expected.add(SimpleGrantedAuthority("ROLE_API"))
+
+        val authorities = granter.getAuthorities(introspectionResponse)
+
+        Assertions.assertTrue(authorities.containsAll(expected))
+        Assertions.assertTrue(expected.containsAll(authorities))
+    }
+}
