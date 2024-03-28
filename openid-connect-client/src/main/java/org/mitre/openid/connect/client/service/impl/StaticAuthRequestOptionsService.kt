c016442eeccdd45ebc7dc8d39553f25321f87946
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 16:05:54 +0000

    Convert StaticAuthRequestOptionsService to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/StaticAuthRequestOptionsService.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/StaticAuthRequestOptionsService.kt
@@ -7,70 +7,60 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service.impl;
+ */
+package org.mitre.openid.connect.client.service.impl
 
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.client.service.AuthRequestOptionsService;
-import org.mitre.openid.connect.config.ServerConfiguration;
-
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.client.service.AuthRequestOptionsService
+import org.mitre.openid.connect.config.ServerConfiguration
+import javax.servlet.http.HttpServletRequest
 
 /**
  *
  * Always returns the same set of options.
  *
  * @author jricher
- *
  */
-public class StaticAuthRequestOptionsService implements AuthRequestOptionsService {
+class StaticAuthRequestOptionsService : AuthRequestOptionsService {
+    private var _options: MutableMap<String, String> = HashMap()
+    var tokenOptions: Map<String, String> = HashMap()
 
-	private Map<String, String> options = new HashMap<>();
-	private Map<String, String> tokenOptions = new HashMap<>();
 
-	/* (non-Javadoc)
+    /**
+     * @return the options object directly
+     */
+    var options: Map<String, String>
+        get() = _options
+        set(value) {
+            this._options = value.toMutableMap()
+        }
+
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.client.service.AuthRequestOptionsService#getOptions(org.mitre.openid.connect.config.ServerConfiguration, org.mitre.oauth2.model.RegisteredClient, javax.servlet.http.HttpServletRequest)
 	 */
-	@Override
-	public Map<String, String> getOptions(ServerConfiguration server, RegisteredClient client, HttpServletRequest request) {
-		return options;
-	}
+    override fun getOptions(
+        server: ServerConfiguration,
+        client: RegisteredClient,
+        request: HttpServletRequest
+    ): MutableMap<String, String> {
+        return _options
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.client.service.AuthRequestOptionsService#getTokenOptions(org.mitre.openid.connect.config.ServerConfiguration, org.mitre.oauth2.model.RegisteredClient, javax.servlet.http.HttpServletRequest)
 	 */
-	@Override
-	public Map<String, String> getTokenOptions(ServerConfiguration server, RegisteredClient client, HttpServletRequest request) {
-		return tokenOptions;
-	}
-
-	/**
-	 * @return the options object directly
-	 */
-	public Map<String, String> getOptions() {
-		return options;
-	}
-
-	public void setOptions(Map<String, String> options) {
-		this.options = options;
-	}
-
-	public Map<String, String> getTokenOptions() {
-		return tokenOptions;
-	}
-
-	public void setTokenOptions(Map<String, String> tokenOptions) {
-		this.tokenOptions = tokenOptions;
-	}
-
+    override fun getTokenOptions(
+        server: ServerConfiguration,
+        client: RegisteredClient,
+        request: HttpServletRequest
+    ): Map<String, String> {
+        return tokenOptions
+    }
 }
