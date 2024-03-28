d8451feff0acaa6449bc97255dfc3e964cc59d15
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 16:33:55 +0000

    Convert HybridClientConfigurationService to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/HybridClientConfigurationService.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/HybridClientConfigurationService.kt
@@ -7,25 +7,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client.service.impl;
+ */
+package org.mitre.openid.connect.client.service.impl
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.mitre.oauth2.model.RegisteredClient;
-import org.mitre.openid.connect.client.service.ClientConfigurationService;
-import org.mitre.openid.connect.client.service.RegisteredClientService;
-import org.mitre.openid.connect.config.ServerConfiguration;
-
-import java.util.Map;
-import java.util.Set;
+import org.mitre.oauth2.model.RegisteredClient
+import org.mitre.openid.connect.client.service.ClientConfigurationService
+import org.mitre.openid.connect.client.service.RegisteredClientService
+import org.mitre.openid.connect.config.ServerConfiguration
 
 /**
  * Houses both a static client configuration and a dynamic client configuration
@@ -36,98 +31,77 @@ import java.util.Set;
  * and blacklist for the dynamic service, and to the static service's client map.
  *
  * @author jricher
- *
  */
-public class HybridClientConfigurationService implements ClientConfigurationService {
+class HybridClientConfigurationService : ClientConfigurationService {
+    private var staticClientService = StaticClientConfigurationService()
 
-	private StaticClientConfigurationService staticClientService = new StaticClientConfigurationService();
+    private var dynamicClientService = DynamicRegistrationClientConfigurationService()
 
-	private DynamicRegistrationClientConfigurationService dynamicClientService = new DynamicRegistrationClientConfigurationService();
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.client.service.ClientConfigurationService#getClientConfiguration(org.mitre.openid.connect.config.ServerConfiguration)
 	 */
-	@Nullable
-  @Override
-	public RegisteredClient getClientConfiguration(@NotNull ServerConfiguration issuer) {
+    override fun getClientConfiguration(issuer: ServerConfiguration): RegisteredClient? {
+        val client = staticClientService.getClientConfiguration(issuer)
+        return client ?: dynamicClientService.getClientConfiguration(issuer)
+    }
 
-		RegisteredClient client = staticClientService.getClientConfiguration(issuer);
-		if (client != null) {
-			return client;
-		} else {
-			return dynamicClientService.getClientConfiguration(issuer);
-		}
+    var clients: Map<String?, RegisteredClient>
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.StaticClientConfigurationService.getClients
+         */
+        get() = staticClientService.clients
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.StaticClientConfigurationService.setClients
+         */
+        set(clients) {
+            staticClientService.clients = clients
+        }
 
-	}
+    var template: RegisteredClient?
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.getTemplate
+         */
+        get() = dynamicClientService.getTemplate()
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.setTemplate
+         */
+        set(template) {
+            dynamicClientService.setTemplate(template)
+        }
 
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.StaticClientConfigurationService#getClients()
-	 */
-	public Map<String, RegisteredClient> getClients() {
-		return staticClientService.getClients();
-	}
+    var registeredClientService: RegisteredClientService?
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.getRegisteredClientService
+         */
+        get() = dynamicClientService.registeredClientService
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.setRegisteredClientService
+         */
+        set(registeredClientService) {
+            dynamicClientService.registeredClientService = registeredClientService!!
+        }
 
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.StaticClientConfigurationService#setClients(java.util.Map)
-	 */
-	public void setClients(Map<String, RegisteredClient> clients) {
-		staticClientService.setClients(clients);
-	}
+    var whitelist: Set<String?>?
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.getWhitelist
+         */
+        get() = dynamicClientService.whitelist
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.setWhitelist
+         */
+        set(whitelist) {
+            dynamicClientService.whitelist = whitelist!!
+        }
 
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#getTemplate()
-	 */
-	public RegisteredClient getTemplate() {
-		return dynamicClientService.getTemplate();
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#setTemplate(org.mitre.oauth2.model.RegisteredClient)
-	 */
-	public void setTemplate(RegisteredClient template) {
-		dynamicClientService.setTemplate(template);
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#getRegisteredClientService()
-	 */
-	public RegisteredClientService getRegisteredClientService() {
-		return dynamicClientService.getRegisteredClientService();
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#setRegisteredClientService(org.mitre.openid.connect.client.service.RegisteredClientService)
-	 */
-	public void setRegisteredClientService(RegisteredClientService registeredClientService) {
-		dynamicClientService.setRegisteredClientService(registeredClientService);
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#getWhitelist()
-	 */
-	public Set<String> getWhitelist() {
-		return dynamicClientService.getWhitelist();
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#setWhitelist(java.util.Set)
-	 */
-	public void setWhitelist(Set<String> whitelist) {
-		dynamicClientService.setWhitelist(whitelist);
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#getBlacklist()
-	 */
-	public Set<String> getBlacklist() {
-		return dynamicClientService.getBlacklist();
-	}
-
-	/**
-	 * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService#setBlacklist(java.util.Set)
-	 */
-	public void setBlacklist(Set<String> blacklist) {
-		dynamicClientService.setBlacklist(blacklist);
-	}
-
+    var blacklist: Set<String?>?
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.getBlacklist
+         */
+        get() = dynamicClientService.blacklist
+        /**
+         * @see org.mitre.openid.connect.client.service.impl.DynamicRegistrationClientConfigurationService.setBlacklist
+         */
+        set(blacklist) {
+            dynamicClientService.blacklist = blacklist!!
+        }
 }
