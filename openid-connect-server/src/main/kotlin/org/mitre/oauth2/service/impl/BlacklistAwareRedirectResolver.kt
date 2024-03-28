62b24ee347a23c46953e0c1df2fa7aea5ce59efd
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 20:15:05 +0000

    Convert BlacklistAwareRedirectResolver to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/service/impl/BlacklistAwareRedirectResolver.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/service/impl/BlacklistAwareRedirectResolver.kt
@@ -5,25 +5,25 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.service.impl;
+ */
+package org.mitre.oauth2.service.impl
 
-import com.google.common.base.Strings;
-import org.mitre.openid.connect.config.ConfigurationPropertiesBean;
-import org.mitre.openid.connect.service.BlacklistedSiteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.common.exceptions.InvalidRequestException;
-import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.endpoint.DefaultRedirectResolver;
-import org.springframework.stereotype.Component;
+import com.google.common.base.Strings
+import org.mitre.openid.connect.config.ConfigurationPropertiesBean
+import org.mitre.openid.connect.service.BlacklistedSiteService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.oauth2.common.exceptions.InvalidRequestException
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
+import org.springframework.security.oauth2.provider.ClientDetails
+import org.springframework.security.oauth2.provider.endpoint.DefaultRedirectResolver
+import org.springframework.stereotype.Component
 
 /**
  *
@@ -31,67 +31,51 @@ import org.springframework.stereotype.Component;
  * for forbidden values. Can be configured to do strict string matching also.
  *
  * @author jricher
- *
  */
 @Component("blacklistAwareRedirectResolver")
-public class BlacklistAwareRedirectResolver extends DefaultRedirectResolver {
+class BlacklistAwareRedirectResolver : DefaultRedirectResolver() {
+    @Autowired
+    private lateinit var blacklistService: BlacklistedSiteService
 
-	@Autowired
-	private BlacklistedSiteService blacklistService;
+    @Autowired
+    private lateinit var config: ConfigurationPropertiesBean
 
-	@Autowired
-	private ConfigurationPropertiesBean config;
+    /**
+     * Set this to true to require exact string matches for all redirect URIs. (Default is false)
+     *
+     */
+    var isStrictMatch: Boolean = true
 
-	private boolean strictMatch = true;
+        get() = config.isHeartMode || field // HEART mode enforces strict matching
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.springframework.security.oauth2.provider.endpoint.RedirectResolver#resolveRedirect(java.lang.String, org.springframework.security.oauth2.provider.ClientDetails)
 	 */
-	@Override
-	public String resolveRedirect(String requestedRedirect, ClientDetails client) throws OAuth2Exception {
-		String redirect = super.resolveRedirect(requestedRedirect, client);
-		if (blacklistService.isBlacklisted(redirect)) {
-			// don't let it go through
-			throw new InvalidRequestException("The supplied redirect_uri is not allowed on this server.");
-		} else {
-			// not blacklisted, passed the parent test, we're fine
-			return redirect;
-		}
-	}
+    @Throws(OAuth2Exception::class)
+    override fun resolveRedirect(requestedRedirect: String, client: ClientDetails): String {
+        val redirect = super.resolveRedirect(requestedRedirect, client)
+        if (blacklistService.isBlacklisted(redirect)) {
+            // don't let it go through
+            throw InvalidRequestException("The supplied redirect_uri is not allowed on this server.")
+        } else {
+            // not blacklisted, passed the parent test, we're fine
+            return redirect
+        }
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.springframework.security.oauth2.provider.endpoint.DefaultRedirectResolver#redirectMatches(java.lang.String, java.lang.String)
 	 */
-	@Override
-	protected boolean redirectMatches(String requestedRedirect, String redirectUri) {
+    public override fun redirectMatches(requestedRedirect: String, redirectUri: String): Boolean {
+        return when {
+            isStrictMatch ->
+                // we're doing a strict string match for all clients
+                Strings.nullToEmpty(requestedRedirect) == redirectUri
 
-		if (isStrictMatch()) {
-			// we're doing a strict string match for all clients
-			return Strings.nullToEmpty(requestedRedirect).equals(redirectUri);
-		} else {
-			// otherwise do the prefix-match from the library
-			return super.redirectMatches(requestedRedirect, redirectUri);
-		}
+            else ->
+                // otherwise do the prefix-match from the library
+                super.redirectMatches(requestedRedirect, redirectUri)
 
-	}
-
-	public boolean isStrictMatch() {
-		if (config.isHeartMode()) {
-			// HEART mode enforces strict matching
-			return true;
-		} else {
-			return strictMatch;
-		}
-	}
-
-	/**
-	 * Set this to true to require exact string matches for all redirect URIs. (Default is false)
-	 *
-	 */
-	public void setStrictMatch(boolean strictMatch) {
-		this.strictMatch = strictMatch;
-	}
-
-
-
+        }
+    }
 }
