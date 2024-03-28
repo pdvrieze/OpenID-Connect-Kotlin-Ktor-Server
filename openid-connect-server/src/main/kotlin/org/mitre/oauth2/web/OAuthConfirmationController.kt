ab8a03c62bff22c08cd43d9c5fa3ed10fd27a486
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 21:43:08 +0000

    Convert OAuthConfirmationController to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/OAuthConfirmationController.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/web/OAuthConfirmationController.kt
@@ -7,225 +7,200 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.web;
+ */
+package org.mitre.oauth2.web
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-import com.google.common.base.Strings;
-import com.google.common.collect.Sets;
-import com.google.gson.JsonObject;
-import org.apache.http.client.utils.URIBuilder;
-import org.mitre.oauth2.model.ClientDetailsEntity;
-import org.mitre.oauth2.model.SystemScope;
-import org.mitre.oauth2.service.ClientDetailsEntityService;
-import org.mitre.oauth2.service.SystemScopeService;
-import org.mitre.openid.connect.model.UserInfo;
-import org.mitre.openid.connect.service.ScopeClaimTranslationService;
-import org.mitre.openid.connect.service.StatsService;
-import org.mitre.openid.connect.service.UserInfoService;
-import org.mitre.openid.connect.view.HttpCodeView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
-import org.springframework.security.oauth2.provider.AuthorizationRequest;
-import org.springframework.security.oauth2.provider.endpoint.RedirectResolver;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import java.net.URISyntaxException;
-import java.security.Principal;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static org.mitre.openid.connect.request.ConnectRequestParameters.PROMPT;
-import static org.mitre.openid.connect.request.ConnectRequestParameters.PROMPT_SEPARATOR;
+import com.google.common.base.Joiner
+import com.google.common.base.Strings
+import com.google.common.collect.Sets
+import org.apache.http.client.utils.URIBuilder
+import org.mitre.oauth2.model.ClientDetailsEntity
+import org.mitre.oauth2.model.SystemScope
+import org.mitre.oauth2.service.ClientDetailsEntityService
+import org.mitre.oauth2.service.SystemScopeService
+import org.mitre.openid.connect.request.ConnectRequestParameters
+import org.mitre.openid.connect.service.ScopeClaimTranslationService
+import org.mitre.openid.connect.service.StatsService
+import org.mitre.openid.connect.service.UserInfoService
+import org.mitre.openid.connect.view.HttpCodeView
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception
+import org.springframework.security.oauth2.provider.AuthorizationRequest
+import org.springframework.security.oauth2.provider.endpoint.RedirectResolver
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.SessionAttributes
+import java.net.URISyntaxException
+import java.security.Principal
+import java.util.*
 
 /**
  * @author jricher
- *
  */
 @Controller
 @SessionAttributes("authorizationRequest")
-public class OAuthConfirmationController {
+class OAuthConfirmationController {
+    @Autowired
+    lateinit var clientService: ClientDetailsEntityService
+
+    @Autowired
+    private lateinit var scopeService: SystemScopeService
+
+    @Autowired
+    private lateinit var scopeClaimTranslationService: ScopeClaimTranslationService
+
+    @Autowired
+    private lateinit var userInfoService: UserInfoService
+
+    @Autowired
+    private lateinit var statsService: StatsService
+
+    @Autowired
+    private lateinit var redirectResolver: RedirectResolver
+
+    constructor()
+
+    constructor(clientService: ClientDetailsEntityService) {
+        this.clientService = clientService
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping("/oauth/confirm_access")
+    fun confirmAccess(model: MutableMap<String?, Any?>, p: Principal): String {
+        val authRequest = model["authorizationRequest"] as AuthorizationRequest?
+
+        // Check the "prompt" parameter to see if we need to do special processing
+        val prompt = authRequest!!.extensions[ConnectRequestParameters.PROMPT] as String?
+        val prompts = prompt?.split(ConnectRequestParameters.PROMPT_SEPARATOR)?: emptyList()
+        var client: ClientDetailsEntity? = null
+
+        try {
+            client = clientService.loadClientByClientId(authRequest.clientId)
+        } catch (e: OAuth2Exception) {
+            logger.error("confirmAccess: OAuth2Exception was thrown when attempting to load client", e)
+            model[HttpCodeView.CODE] = HttpStatus.BAD_REQUEST
+            return HttpCodeView.VIEWNAME
+        } catch (e: IllegalArgumentException) {
+            logger.error("confirmAccess: IllegalArgumentException was thrown when attempting to load client", e)
+            model[HttpCodeView.CODE] = HttpStatus.BAD_REQUEST
+            return HttpCodeView.VIEWNAME
+        }
+
+        if (client == null) {
+            logger.error("confirmAccess: could not find client " + authRequest.clientId)
+            model[HttpCodeView.CODE] = HttpStatus.NOT_FOUND
+            return HttpCodeView.VIEWNAME
+        }
+
+        if (prompts.contains("none")) {
+            // if we've got a redirect URI then we'll send it
+
+            val url = redirectResolver.resolveRedirect(authRequest.redirectUri, client)
+
+            try {
+                val uriBuilder = URIBuilder(url)
+
+                uriBuilder.addParameter("error", "interaction_required")
+                if (!Strings.isNullOrEmpty(authRequest.state)) {
+                    uriBuilder.addParameter("state", authRequest.state) // copy the state parameter if one was given
+                }
+
+                return "redirect:$uriBuilder"
+            } catch (e: URISyntaxException) {
+                logger.error("Can't build redirect URI for prompt=none, sending error instead", e)
+                model["code"] = HttpStatus.FORBIDDEN
+                return HttpCodeView.VIEWNAME
+            }
+        }
+
+        model["auth_request"] = authRequest
+        model["client"] = client
+
+        val redirect_uri = authRequest.redirectUri
+
+        model["redirect_uri"] = redirect_uri
 
 
-	@Autowired
-	private ClientDetailsEntityService clientService;
+        // pre-process the scopes
+        val scopes: Set<SystemScope?>? = scopeService.fromStrings(authRequest.scope)
 
-	@Autowired
-	private SystemScopeService scopeService;
+        val sortedScopes: MutableSet<SystemScope?> = LinkedHashSet(scopes!!.size)
+        val systemScopes: Set<SystemScope?> = scopeService.all
 
-	@Autowired
-	private ScopeClaimTranslationService scopeClaimTranslationService;
+        // sort scopes for display based on the inherent order of system scopes
+        for (s in systemScopes) {
+            if (scopes.contains(s)) {
+                sortedScopes.add(s)
+            }
+        }
 
-	@Autowired
-	private UserInfoService userInfoService;
+        // add in any scopes that aren't system scopes to the end of the list
+        sortedScopes.addAll(Sets.difference(scopes, systemScopes))
 
-	@Autowired
-	private StatsService statsService;
+        model["scopes"] = sortedScopes
 
-	@Autowired
-	private RedirectResolver redirectResolver;
+        // get the userinfo claims for each scope
+        val user = userInfoService.getByUsername(p.name)
+        val claimsForScopes: MutableMap<String?, Map<String, String>> = HashMap()
+        if (user != null) {
+            val userJson = user.toJson()
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(OAuthConfirmationController.class);
+            for (systemScope in sortedScopes) {
+                val claimValues: MutableMap<String, String> = HashMap()
 
-	public OAuthConfirmationController() {
+                val claims = scopeClaimTranslationService.getClaimsForScope(systemScope!!.value!!)
+                for (claim in claims!!) {
+                    if (userJson!!.has(claim) && userJson[claim].isJsonPrimitive) {
+                        // TODO: this skips the address claim
+                        claimValues[claim] = userJson[claim].asString
+                    }
+                }
 
-	}
+                claimsForScopes[systemScope.value] = claimValues
+            }
+        }
 
-	public OAuthConfirmationController(ClientDetailsEntityService clientService) {
-		this.clientService = clientService;
-	}
+        model["claims"] = claimsForScopes
 
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping("/oauth/confirm_access")
-	public String confirmAccess(Map<String, Object> model, Principal p) {
-
-		AuthorizationRequest authRequest = (AuthorizationRequest) model.get("authorizationRequest");
-		// Check the "prompt" parameter to see if we need to do special processing
-
-		String prompt = (String)authRequest.getExtensions().get(PROMPT);
-		List<String> prompts = Splitter.on(PROMPT_SEPARATOR).splitToList(Strings.nullToEmpty(prompt));
-		ClientDetailsEntity client = null;
-
-		try {
-			client = clientService.loadClientByClientId(authRequest.getClientId());
-		} catch (OAuth2Exception e) {
-			logger.error("confirmAccess: OAuth2Exception was thrown when attempting to load client", e);
-			model.put(HttpCodeView.CODE, HttpStatus.BAD_REQUEST);
-			return HttpCodeView.VIEWNAME;
-		} catch (IllegalArgumentException e) {
-			logger.error("confirmAccess: IllegalArgumentException was thrown when attempting to load client", e);
-			model.put(HttpCodeView.CODE, HttpStatus.BAD_REQUEST);
-			return HttpCodeView.VIEWNAME;
-		}
-
-		if (client == null) {
-			logger.error("confirmAccess: could not find client " + authRequest.getClientId());
-			model.put(HttpCodeView.CODE, HttpStatus.NOT_FOUND);
-			return HttpCodeView.VIEWNAME;
-		}
-
-		if (prompts.contains("none")) {
-			// if we've got a redirect URI then we'll send it
-
-			String url = redirectResolver.resolveRedirect(authRequest.getRedirectUri(), client);
-
-			try {
-				URIBuilder uriBuilder = new URIBuilder(url);
-
-				uriBuilder.addParameter("error", "interaction_required");
-				if (!Strings.isNullOrEmpty(authRequest.getState())) {
-					uriBuilder.addParameter("state", authRequest.getState()); // copy the state parameter if one was given
-				}
-
-				return "redirect:" + uriBuilder.toString();
-
-			} catch (URISyntaxException e) {
-				logger.error("Can't build redirect URI for prompt=none, sending error instead", e);
-				model.put("code", HttpStatus.FORBIDDEN);
-				return HttpCodeView.VIEWNAME;
-			}
-		}
-
-		model.put("auth_request", authRequest);
-		model.put("client", client);
-
-		String redirect_uri = authRequest.getRedirectUri();
-
-		model.put("redirect_uri", redirect_uri);
+        // client stats
+        val count = statsService.getCountForClientId(client.clientId!!)!!.approvedSiteCount
+        model["count"] = count
 
 
-		// pre-process the scopes
-		Set<SystemScope> scopes = scopeService.fromStrings(authRequest.getScope());
+        // contacts
+        if (client.contacts != null) {
+            val contacts = Joiner.on(", ").join(client.contacts)
+            model["contacts"] = contacts
+        }
 
-		Set<SystemScope> sortedScopes = new LinkedHashSet<>(scopes.size());
-		Set<SystemScope> systemScopes = scopeService.getAll();
+        // if the client is over a week old and has more than one registration, don't give such a big warning
+        // instead, tag as "Generally Recognized As Safe" (gras)
+        val lastWeek = Date(System.currentTimeMillis() - (60 * 60 * 24 * 7 * 1000))
+        if (count!! > 1 && client.createdAt != null && client.createdAt!!.before(lastWeek)) {
+            model["gras"] = true
+        } else {
+            model["gras"] = false
+        }
 
-		// sort scopes for display based on the inherent order of system scopes
-		for (SystemScope s : systemScopes) {
-			if (scopes.contains(s)) {
-				sortedScopes.add(s);
-			}
-		}
-
-		// add in any scopes that aren't system scopes to the end of the list
-		sortedScopes.addAll(Sets.difference(scopes, systemScopes));
-
-		model.put("scopes", sortedScopes);
-
-		// get the userinfo claims for each scope
-		UserInfo user = userInfoService.getByUsername(p.getName());
-		Map<String, Map<String, String>> claimsForScopes = new HashMap<>();
-		if (user != null) {
-			JsonObject userJson = user.toJson();
-
-			for (SystemScope systemScope : sortedScopes) {
-				Map<String, String> claimValues = new HashMap<>();
-
-				Set<String> claims = scopeClaimTranslationService.getClaimsForScope(systemScope.getValue());
-				for (String claim : claims) {
-					if (userJson.has(claim) && userJson.get(claim).isJsonPrimitive()) {
-						// TODO: this skips the address claim
-						claimValues.put(claim, userJson.get(claim).getAsString());
-					}
-				}
-
-				claimsForScopes.put(systemScope.getValue(), claimValues);
-			}
-		}
-
-		model.put("claims", claimsForScopes);
-
-		// client stats
-		Integer count = statsService.getCountForClientId(client.getClientId()).getApprovedSiteCount();
-		model.put("count", count);
+        return "approve"
+    }
 
 
-		// contacts
-		if (client.getContacts() != null) {
-			String contacts = Joiner.on(", ").join(client.getContacts());
-			model.put("contacts", contacts);
-		}
-
-		// if the client is over a week old and has more than one registration, don't give such a big warning
-		// instead, tag as "Generally Recognized As Safe" (gras)
-		Date lastWeek = new Date(System.currentTimeMillis() - (60 * 60 * 24 * 7 * 1000));
-		if (count > 1 && client.getCreatedAt() != null && client.getCreatedAt().before(lastWeek)) {
-			model.put("gras", true);
-		} else {
-			model.put("gras", false);
-		}
-
-		return "approve";
-	}
-
-	public ClientDetailsEntityService getClientService() {
-		return clientService;
-	}
-
-	public void setClientService(ClientDetailsEntityService clientService) {
-		this.clientService = clientService;
-	}
-
-
+    companion object {
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(OAuthConfirmationController::class.java)
+    }
 }
