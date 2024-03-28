ce9595a43de9ab98ec3c0d52b5554b60929fb4c4
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 22:52:18 +0000

    Convert the remaining web classes to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/RootController.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/RootController.kt
@@ -7,72 +7,61 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.web;
+ */
+package org.mitre.openid.connect.web
 
-import java.util.Map;
-
-import org.mitre.openid.connect.service.StatsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.mitre.openid.connect.service.StatsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.RequestMapping
 
 /**
- * @author Michael Jett <mjett@mitre.org>
+ * @author Michael Jett <mjett></mjett>@mitre.org>
  */
-
 @Controller
-public class RootController {
+class RootController {
+    @Autowired
+    var statsService: StatsService? = null
 
-	public static final String API_URL = "api";
+    @RequestMapping(value = ["", "home", "index"])
+    fun showHomePage(m: ModelMap?): String {
+        return "home"
+    }
 
-	@Autowired
-	private StatsService statsService;
+    @RequestMapping(value = ["about", "about/"])
+    fun showAboutPage(m: ModelMap?): String {
+        return "about"
+    }
 
-	@RequestMapping({"", "home", "index"})
-	public String showHomePage(ModelMap m) {
-		return "home";
-	}
+    @RequestMapping(value = ["stats", "stats/"])
+    fun showStatsPage(m: ModelMap): String {
+        val summary = statsService!!.summaryStats
 
-	@RequestMapping({"about", "about/"})
-	public String showAboutPage(ModelMap m) {
-		return "about";
-	}
+        m["statsSummary"] = summary
+        return "stats"
+    }
 
-	@RequestMapping({"stats", "stats/"})
-	public String showStatsPage(ModelMap m) {
-		Map<String, Integer> summary = statsService.getSummaryStats();
+    @RequestMapping(value = ["contact", "contact/"])
+    fun showContactPage(m: ModelMap?): String {
+        return "contact"
+    }
 
-		m.put("statsSummary", summary);
-		return "stats";
-	}
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping("manage/**")
+    fun showClientManager(m: ModelMap?): String {
+        return "manage"
+    }
 
-	@RequestMapping({"contact", "contact/"})
-	public String showContactPage(ModelMap m) {
-		return "contact";
-	}
-
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping("manage/**")
-	public String showClientManager(ModelMap m) {
-		return "manage";
-	}
-
-	public StatsService getStatsService() {
-		return statsService;
-	}
-
-	public void setStatsService(StatsService statsService) {
-		this.statsService = statsService;
-	}
-
+    companion object {
+        const val API_URL: String = "api"
+    }
 }
--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/StatsAPI.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/StatsAPI.kt
@@ -7,72 +7,68 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.web;
+ */
+package org.mitre.openid.connect.web
 
-import java.util.Map;
-
-import org.mitre.openid.connect.model.ClientStat;
-import org.mitre.openid.connect.service.StatsService;
-import org.mitre.openid.connect.view.JsonEntityView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.mitre.openid.connect.service.StatsService
+import org.mitre.openid.connect.view.JsonEntityView
+import org.mitre.openid.connect.web.StatsAPI
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/" + StatsAPI.URL)
-public class StatsAPI {
+class StatsAPI {
+    @Autowired
+    private lateinit var statsService: StatsService
 
-	public static final String URL = RootController.API_URL + "/stats";
+    @RequestMapping(value = ["summary"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun statsSummary(m: ModelMap): String {
+        val e = statsService.summaryStats
 
-	// Logger for this class
-	private static final Logger logger = LoggerFactory.getLogger(StatsAPI.class);
+        m[JsonEntityView.ENTITY] = e
 
-	@Autowired
-	private StatsService statsService;
+        return JsonEntityView.VIEWNAME
+    }
 
-	@RequestMapping(value = "summary", produces = MediaType.APPLICATION_JSON_VALUE)
-	public String statsSummary(ModelMap m) {
+    //	@PreAuthorize("hasRole('ROLE_USER')")
+    //	@RequestMapping(value = "byclientid", produces = MediaType.APPLICATION_JSON_VALUE)
+    //	public String statsByClient(ModelMap m) {
+    //		Map<Long, Integer> e = statsService.getByClientId();
+    //
+    //		m.put(JsonEntityView.ENTITY, e);
+    //
+    //		return JsonEntityView.VIEWNAME;
+    //	}
+    //
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = ["byclientid/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun statsByClientId(@PathVariable("id") clientId: String?, m: ModelMap): String {
+        val e = statsService.getCountForClientId(clientId!!)
 
-		Map<String, Integer> e = statsService.getSummaryStats();
+        m[JsonEntityView.ENTITY] = e
 
-		m.put(JsonEntityView.ENTITY, e);
+        return JsonEntityView.VIEWNAME
+    }
 
-		return JsonEntityView.VIEWNAME;
+    companion object {
+        const val URL: String = RootController.API_URL + "/stats"
 
-	}
-
-	//	@PreAuthorize("hasRole('ROLE_USER')")
-	//	@RequestMapping(value = "byclientid", produces = MediaType.APPLICATION_JSON_VALUE)
-	//	public String statsByClient(ModelMap m) {
-	//		Map<Long, Integer> e = statsService.getByClientId();
-	//
-	//		m.put(JsonEntityView.ENTITY, e);
-	//
-	//		return JsonEntityView.VIEWNAME;
-	//	}
-	//
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping(value = "byclientid/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public String statsByClientId(@PathVariable("id") String clientId, ModelMap m) {
-		ClientStat e = statsService.getCountForClientId(clientId);
-
-		m.put(JsonEntityView.ENTITY, e);
-
-		return JsonEntityView.VIEWNAME;
-	}
-
+        // Logger for this class
+        private val logger: Logger = LoggerFactory.getLogger(StatsAPI::class.java)
+    }
 }
