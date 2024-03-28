28e95d6236b07ac11398e5fcea4ed821fc5626c3
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 22:43:40 +0000

    Convert JWKSetPublishingEndpoint to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/JWKSetPublishingEndpoint.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/web/JWKSetPublishingEndpoint.kt
@@ -7,54 +7,42 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.web;
+ */
+package org.mitre.openid.connect.web
 
-import com.nimbusds.jose.jwk.JWK;
-import org.mitre.jwt.signer.service.JWTSigningAndValidationService;
-import org.mitre.openid.connect.view.JWKSetView;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
+import org.mitre.jwt.signer.service.JWTSigningAndValidationService
+import org.mitre.openid.connect.view.JWKSetView
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-public class JWKSetPublishingEndpoint {
+class JWKSetPublishingEndpoint {
+    @Autowired
+    lateinit var jwtService: JWTSigningAndValidationService
 
-	public static final String URL = "jwk";
+    @RequestMapping(value = ["/" + URL], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getJwk(m: Model): String {
+        // map from key id to key
 
-	@Autowired
-	private JWTSigningAndValidationService jwtService;
+        val keys = jwtService.allPublicKeys
 
-	@RequestMapping(value = "/" + URL, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getJwk(Model m) {
+        // TODO: check if keys are empty, return a 404 here or just an empty list?
+        m.addAttribute("keys", keys)
 
-		// map from key id to key
-		Map<String, JWK> keys = jwtService.getAllPublicKeys();
+        return JWKSetView.VIEWNAME
+    }
 
-		// TODO: check if keys are empty, return a 404 here or just an empty list?
-
-		m.addAttribute("keys", keys);
-
-		return JWKSetView.VIEWNAME;
-	}
-
-	public JWTSigningAndValidationService getJwtService() {
-		return jwtService;
-	}
-
-	public void setJwtService(JWTSigningAndValidationService jwtService) {
-		this.jwtService = jwtService;
-	}
-
+    companion object {
+        const val URL: String = "jwk"
+    }
 }
