1a55644e9ee0edda1dba3fcebc0f6cef7c66df92
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 15:25:07 +0000

    Convert OIDCAuthenticationProvider to Kotlin.

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/OIDCAuthenticationProvider.kt
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/OIDCAuthenticationProvider.kt
@@ -7,113 +7,113 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.client;
+ */
+package org.mitre.openid.connect.client
 
-import com.google.common.base.Strings;
-import com.nimbusds.jwt.JWT;
-import org.mitre.openid.connect.model.OIDCAuthenticationToken;
-import org.mitre.openid.connect.model.PendingOIDCAuthenticationToken;
-import org.mitre.openid.connect.model.UserInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Collection;
+import com.google.common.base.Strings
+import org.mitre.openid.connect.model.OIDCAuthenticationToken
+import org.mitre.openid.connect.model.PendingOIDCAuthenticationToken
+import org.mitre.openid.connect.model.UserInfo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 
 /**
  * @author nemonik, Justin Richer
- *
  */
-public class OIDCAuthenticationProvider implements AuthenticationProvider {
+class OIDCAuthenticationProvider : AuthenticationProvider {
+    private var userInfoFetcher = UserInfoFetcher()
 
-	private static Logger logger = LoggerFactory.getLogger(OIDCAuthenticationProvider.class);
+    private var authoritiesMapper: OIDCAuthoritiesMapper = NamedAdminAuthoritiesMapper()
 
-	private UserInfoFetcher userInfoFetcher = new UserInfoFetcher();
-
-	private OIDCAuthoritiesMapper authoritiesMapper = new NamedAdminAuthoritiesMapper();
-
-	/*
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see org.springframework.security.authentication.AuthenticationProvider#
 	 * authenticate(org.springframework.security.core.Authentication)
 	 */
-	@Override
-	public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+    @Throws(AuthenticationException::class)
+    override fun authenticate(authentication: Authentication): Authentication? {
+        if (!supports(authentication.javaClass)) {
+            return null
+        }
 
-		if (!supports(authentication.getClass())) {
-			return null;
-		}
+        if (authentication is PendingOIDCAuthenticationToken) {
+            val token = authentication
 
-		if (authentication instanceof PendingOIDCAuthenticationToken) {
+            // get the ID Token value out
+            val idToken = token.idToken
 
-			PendingOIDCAuthenticationToken token = (PendingOIDCAuthenticationToken) authentication;
+            // load the user info if we can
+            val userInfo = userInfoFetcher.loadUserInfo(token)
 
-			// get the ID Token value out
-			JWT idToken = token.getIdToken();
+            if (userInfo == null) {
+                // user info not found -- could be an error, could be fine
+            } else {
+                // if we found userinfo, double check it
+                if (!Strings.isNullOrEmpty(userInfo.sub) && userInfo.sub != token.sub) {
+                    // the userinfo came back and the user_id fields don't match what was in the id_token
+                    throw UsernameNotFoundException("user_id mismatch between id_token and user_info call: " + token.sub + " / " + userInfo.sub)
+                }
+            }
 
-			// load the user info if we can
-			UserInfo userInfo = userInfoFetcher.loadUserInfo(token);
+            return createAuthenticationToken(token, authoritiesMapper.mapAuthorities(idToken!!, userInfo), userInfo)
+        }
 
-			if (userInfo == null) {
-				// user info not found -- could be an error, could be fine
-			} else {
-				// if we found userinfo, double check it
-				if (!Strings.isNullOrEmpty(userInfo.getSub()) && !userInfo.getSub().equals(token.getSub())) {
-					// the userinfo came back and the user_id fields don't match what was in the id_token
-					throw new UsernameNotFoundException("user_id mismatch between id_token and user_info call: " + token.getSub() + " / " + userInfo.getSub());
-				}
-			}
+        return null
+    }
 
-			return createAuthenticationToken(token, authoritiesMapper.mapAuthorities(idToken, userInfo), userInfo);
-		}
-
-		return null;
-	}
-
-	/**
-	 * Override this function to return a different kind of Authentication, processes the authorities differently,
-	 * or do post-processing based on the UserInfo object.
-	 *
-	 */
-	protected Authentication createAuthenticationToken(PendingOIDCAuthenticationToken token, Collection<? extends GrantedAuthority> authorities, UserInfo userInfo) {
-		return new OIDCAuthenticationToken(token.getSub(),
-				token.getIssuer(),
-				userInfo, authorities,
-				token.getIdToken(), token.getAccessTokenValue(), token.getRefreshTokenValue());
-	}
-
-
-	public void setUserInfoFetcher(UserInfoFetcher userInfoFetcher) {
-		this.userInfoFetcher = userInfoFetcher;
-	}
+    /**
+     * Override this function to return a different kind of Authentication, processes the authorities differently,
+     * or do post-processing based on the UserInfo object.
+     *
+     */
+    protected fun createAuthenticationToken(
+        token: PendingOIDCAuthenticationToken,
+        authorities: Collection<GrantedAuthority>?,
+        userInfo: UserInfo?
+    ): Authentication {
+        return OIDCAuthenticationToken(
+            token.sub,
+            token.issuer,
+            userInfo, authorities,
+            token.idToken, token.accessTokenValue, token.refreshTokenValue
+        )
+    }
 
 
-	public void setAuthoritiesMapper(OIDCAuthoritiesMapper authoritiesMapper) {
-		this.authoritiesMapper = authoritiesMapper;
-	}
+    fun setUserInfoFetcher(userInfoFetcher: UserInfoFetcher) {
+        this.userInfoFetcher = userInfoFetcher
+    }
 
-	/*
+
+    fun setAuthoritiesMapper(authoritiesMapper: OIDCAuthoritiesMapper) {
+        this.authoritiesMapper = authoritiesMapper
+    }
+
+    /*
 	 * (non-Javadoc)
 	 *
 	 * @see
 	 * org.springframework.security.authentication.AuthenticationProvider#supports
 	 * (java.lang.Class)
 	 */
-	@Override
-	public boolean supports(Class<?> authentication) {
-		return PendingOIDCAuthenticationToken.class.isAssignableFrom(authentication);
-	}
+    override fun supports(authentication: Class<*>?): Boolean {
+        return PendingOIDCAuthenticationToken::class.java.isAssignableFrom(authentication)
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(OIDCAuthenticationProvider::class.java)
+    }
 }
