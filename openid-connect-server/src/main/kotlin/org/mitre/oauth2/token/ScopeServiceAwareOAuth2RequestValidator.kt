bb3d5b765d114339765dc68c37b6d6690e3a38e9
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-28 19:48:34 +0000

    Convert ScopeServiceAwareOAuth2RequestValidator to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/oauth2/token/ScopeServiceAwareOAuth2RequestValidator.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/oauth2/token/ScopeServiceAwareOAuth2RequestValidator.kt
@@ -7,25 +7,24 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.oauth2.token;
+ */
+package org.mitre.oauth2.token
 
-import org.mitre.oauth2.service.SystemScopeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.common.exceptions.InvalidScopeException;
-import org.springframework.security.oauth2.provider.AuthorizationRequest;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.OAuth2RequestValidator;
-import org.springframework.security.oauth2.provider.TokenRequest;
-
-import java.util.Set;
+import org.mitre.oauth2.service.SystemScopeService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.oauth2.common.exceptions.InvalidScopeException
+import org.springframework.security.oauth2.provider.AuthorizationRequest
+import org.springframework.security.oauth2.provider.ClientDetails
+import org.springframework.security.oauth2.provider.OAuth2RequestValidator
+import org.springframework.security.oauth2.provider.TokenRequest
+import org.springframework.stereotype.Component
 
 /**
  *
@@ -33,34 +32,30 @@ import java.util.Set;
  * allowed scopes, but allow custom scopes to function through the system scopes
  *
  * @author jricher
- *
  */
-public class ScopeServiceAwareOAuth2RequestValidator implements OAuth2RequestValidator {
+@Component
+class ScopeServiceAwareOAuth2RequestValidator : OAuth2RequestValidator {
+    @Autowired
+    private lateinit var scopeService: SystemScopeService
 
-	@Autowired
-	private SystemScopeService scopeService;
+    @Throws(InvalidScopeException::class)
+    private fun validateScope(requestedScopes: Set<String>?, clientScopes: Set<String>?) {
+        if (!requestedScopes.isNullOrEmpty()) {
+            if (!clientScopes.isNullOrEmpty()) {
+                if (!scopeService.scopesMatch(clientScopes, requestedScopes)) {
+                    throw InvalidScopeException("Invalid scope; requested:$requestedScopes", clientScopes)
+                }
+            }
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.security.oauth2.provider.OAuth2RequestValidator#validateScope(java.util.Map, java.util.Set)
-	 */
-	private void validateScope(Set<String> requestedScopes, Set<String> clientScopes) throws InvalidScopeException {
-		if (requestedScopes != null && !requestedScopes.isEmpty()) {
-			if (clientScopes != null && !clientScopes.isEmpty()) {
-				if (!scopeService.scopesMatch(clientScopes, requestedScopes)) {
-					throw new InvalidScopeException("Invalid scope; requested:" + requestedScopes, clientScopes);
-				}
-			}
-		}
-	}
+    @Throws(InvalidScopeException::class)
+    override fun validateScope(authorizationRequest: AuthorizationRequest, client: ClientDetails) {
+        validateScope(authorizationRequest.scope, client.scope)
+    }
 
-	@Override
-	public void validateScope(AuthorizationRequest authorizationRequest, ClientDetails client) throws InvalidScopeException {
-		validateScope(authorizationRequest.getScope(), client.getScope());
-	}
-
-	@Override
-	public void validateScope(TokenRequest tokenRequest, ClientDetails client) throws InvalidScopeException {
-		validateScope(tokenRequest.getScope(), client.getScope());
-	}
-
+    @Throws(InvalidScopeException::class)
+    override fun validateScope(tokenRequest: TokenRequest, client: ClientDetails) {
+        validateScope(tokenRequest.scope, client.scope)
+    }
 }
