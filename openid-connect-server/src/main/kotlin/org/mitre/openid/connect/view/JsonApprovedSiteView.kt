62610271f3c0fa8e7f6e5b391c0376f67619e30a
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 11:54:07 +0000

    Convert JsonApprovedSiteView to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonApprovedSiteView.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonApprovedSiteView.kt
@@ -7,116 +7,85 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.view;
+ */
+package org.mitre.openid.connect.view
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
-import org.mitre.openid.connect.model.WhitelistedSite;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.web.servlet.view.AbstractView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.reflect.Type;
-import java.util.Map;
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializer
+import org.mitre.oauth2.model.OAuth2AccessTokenEntity
+import org.mitre.openid.connect.model.WhitelistedSite
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.web.servlet.view.AbstractView
+import java.io.IOException
+import java.io.Writer
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * @author jricher
- *
  */
 @Component(JsonApprovedSiteView.VIEWNAME)
-public class JsonApprovedSiteView extends AbstractView {
+class JsonApprovedSiteView : AbstractView() {
+    private val gson: Gson = GsonBuilder()
+        .setExclusionStrategies(object : ExclusionStrategy {
+            override fun shouldSkipField(f: FieldAttributes): Boolean {
+                return false
+            }
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(JsonApprovedSiteView.class);
+            override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                // skip the JPA binding wrapper
+                return clazz == BeanPropertyBindingResult::class.java
+            }
+        })
+        .registerTypeAdapter(OAuth2AccessTokenEntity::class.java, JsonSerializer<OAuth2AccessTokenEntity> { src, typeOfSrc, context -> JsonPrimitive(src.id) })
+        .registerTypeAdapter(WhitelistedSite::class.java, JsonSerializer<WhitelistedSite> { src, typeOfSrc, context -> JsonPrimitive(src.id) })
+        .serializeNulls()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+        .create()
 
-	public static final String VIEWNAME = "jsonApprovedSiteView";
-
-	private Gson gson = new GsonBuilder()
-			.setExclusionStrategies(new ExclusionStrategy() {
-
-				@Override
-				public boolean shouldSkipField(FieldAttributes f) {
-
-					return false;
-				}
-
-				@Override
-				public boolean shouldSkipClass(Class<?> clazz) {
-					// skip the JPA binding wrapper
-					if (clazz.equals(BeanPropertyBindingResult.class)) {
-						return true;
-					}
-					return false;
-				}
-
-			})
-			.registerTypeAdapter(OAuth2AccessTokenEntity.class, new JsonSerializer<OAuth2AccessTokenEntity>() {
-				@Override
-				public JsonElement serialize(OAuth2AccessTokenEntity src,
-						Type typeOfSrc, JsonSerializationContext context) {
-					return new JsonPrimitive(src.getId());
-				}
-			})
-			.registerTypeAdapter(WhitelistedSite.class, new JsonSerializer<WhitelistedSite>() {
-				@Override
-				public JsonElement serialize(WhitelistedSite src, Type typeOfSrc, JsonSerializationContext context) {
-					return new JsonPrimitive(src.getId());
-				}
-			})
-			.serializeNulls()
-			.setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-			.create();
-
-	@Override
-	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) {
-
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    override fun renderMergedOutputModel(
+        model: Map<String, Any>,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
 
 
-		HttpStatus code = (HttpStatus) model.get(HttpCodeView.CODE);
-		if (code == null) {
-			code = HttpStatus.OK; // default to 200
-		}
+        val code = model[HttpCodeView.CODE] as HttpStatus? ?: HttpStatus.OK
 
-		response.setStatus(code.value());
+        response.setStatus(code.value())
 
-		try {
+        try {
+            val out: Writer = response.writer
+            val obj = model[JsonEntityView.ENTITY]
+            gson.toJson(obj, out)
+        } catch (e: IOException) {
+            Companion.logger.error("IOException in JsonEntityView.java: ", e)
+        }
+    }
 
-			Writer out = response.getWriter();
-			Object obj = model.get(JsonEntityView.ENTITY);
-			gson.toJson(obj, out);
+    companion object {
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(JsonApprovedSiteView::class.java)
 
-		} catch (IOException e) {
-
-			logger.error("IOException in JsonEntityView.java: ", e);
-
-		}
-	}
-
+        const val VIEWNAME: String = "jsonApprovedSiteView"
+    }
 }
