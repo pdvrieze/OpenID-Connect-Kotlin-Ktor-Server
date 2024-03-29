fc51ac7135a4fdb7073eee5b09704d894eb38fa0
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 17:49:51 +0000

    Convert MultiUrlRequestMatcher to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/filter/MultiUrlRequestMatcher.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/filter/MultiUrlRequestMatcher.kt
@@ -5,53 +5,37 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.openid.connect.filter
 
-package org.mitre.openid.connect.filter;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.security.web.util.UrlUtils;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.Assert;
+import org.springframework.security.web.util.UrlUtils
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
+import javax.servlet.http.HttpServletRequest
 
 /**
  * @author jricher
- *
  */
-public class MultiUrlRequestMatcher implements RequestMatcher {
-	private final Set<RequestMatcher> matchers;
+class MultiUrlRequestMatcher(filterProcessesUrls: Set<String>) : RequestMatcher {
+    private val matchers: MutableSet<RequestMatcher> =
+        HashSet(filterProcessesUrls.size)
 
-	public MultiUrlRequestMatcher(Set<String> filterProcessesUrls) {
-		this.matchers = new HashSet<>(filterProcessesUrls.size());
-		for (String filterProcessesUrl : filterProcessesUrls) {
-			Assert.hasLength(filterProcessesUrl, "filterProcessesUrl must be specified");
-			Assert.isTrue(UrlUtils.isValidRedirectUrl(filterProcessesUrl), filterProcessesUrl + " isn't a valid URL");
-			matchers.add(new AntPathRequestMatcher(filterProcessesUrl));
-		}
+    init {
+        for (filterProcessesUrl in filterProcessesUrls) {
+            require(filterProcessesUrl.isNotEmpty()) { "filterProcessesUrl must be specified" }
+            require(UrlUtils.isValidRedirectUrl(filterProcessesUrl)) { "$filterProcessesUrl isn't a valid URL" }
+            matchers.add(AntPathRequestMatcher(filterProcessesUrl))
+        }
+    }
 
-	}
-
-	@Override
-	public boolean matches(HttpServletRequest request) {
-		for (RequestMatcher matcher : matchers) {
-			if (matcher.matches(request)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
+    override fun matches(request: HttpServletRequest): Boolean {
+        return matchers.any { it.matches(request) }
+    }
 }
