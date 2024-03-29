ef117d55ee10515085ea849fba6110d143a7208a
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 11:52:41 +0000

    Convert JsonEntityView to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonEntityView.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonEntityView.kt
@@ -7,99 +7,86 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.view;
+ */
+package org.mitre.openid.connect.view
 
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.web.servlet.view.AbstractView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Map;
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.mitre.openid.connect.view.JsonEntityView
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.web.servlet.view.AbstractView
+import java.io.IOException
+import java.io.Writer
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * @author jricher
- *
  */
 @Component(JsonEntityView.VIEWNAME)
-public class JsonEntityView extends AbstractView {
+class JsonEntityView : AbstractView() {
+    private val gson: Gson = GsonBuilder()
+        .setExclusionStrategies(object : ExclusionStrategy {
+            override fun shouldSkipField(f: FieldAttributes): Boolean {
+                return false
+            }
 
-	public static final String ENTITY = "entity";
+            override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                // skip the JPA binding wrapper
+                if (clazz == BeanPropertyBindingResult::class.java) {
+                    return true
+                }
+                return false
+            }
+        })
+        .serializeNulls()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+        .create()
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(JsonEntityView.class);
+    override fun renderMergedOutputModel(
+        model: Map<String, Any>,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
 
-	public static final String VIEWNAME = "jsonEntityView";
+        // default to 200
+        val code = model[HttpCodeView.CODE] as HttpStatus? ?: HttpStatus.OK
 
-	private Gson gson = new GsonBuilder()
-			.setExclusionStrategies(new ExclusionStrategy() {
+        response.setStatus(code.value())
 
-				@Override
-				public boolean shouldSkipField(FieldAttributes f) {
+        try {
+            val out: Writer = response.writer
+            val obj = model[ENTITY]
+            gson.toJson(obj, out)
+        } catch (e: IOException) {
+            Companion.logger.error("IOException in JsonEntityView.java: ", e)
+        }
+    }
 
-					return false;
-				}
+    companion object {
+        const val ENTITY: String = "entity"
 
-				@Override
-				public boolean shouldSkipClass(Class<?> clazz) {
-					// skip the JPA binding wrapper
-					if (clazz.equals(BeanPropertyBindingResult.class)) {
-						return true;
-					}
-					return false;
-				}
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(JsonEntityView::class.java)
 
-			})
-			.serializeNulls()
-			.setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-			.create();
-
-	@Override
-	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) {
-
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		response.setCharacterEncoding("UTF-8");
-
-
-		HttpStatus code = (HttpStatus) model.get(HttpCodeView.CODE);
-		if (code == null) {
-			code = HttpStatus.OK; // default to 200
-		}
-
-		response.setStatus(code.value());
-
-		try {
-
-			Writer out = response.getWriter();
-			Object obj = model.get(ENTITY);
-			gson.toJson(obj, out);
-
-		} catch (IOException e) {
-
-			logger.error("IOException in JsonEntityView.java: ", e);
-
-		}
-	}
-
+        const val VIEWNAME: String = "jsonEntityView"
+    }
 }
