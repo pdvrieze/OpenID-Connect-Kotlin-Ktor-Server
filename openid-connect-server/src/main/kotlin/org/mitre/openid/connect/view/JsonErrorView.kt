e6c538b2ce5ead20d2bc583842e124243310a27f
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 11:51:45 +0000

    Convert JsonErrorView to Kotlin

--- openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonErrorView.kt
+++ openid-connect-server/src/main/kotlin/org/mitre/openid/connect/view/JsonErrorView.kt
@@ -7,112 +7,101 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.view;
+ */
+package org.mitre.openid.connect.view
 
-import com.google.common.base.Strings;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.web.servlet.view.AbstractView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Map;
+import com.google.common.base.Strings
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import org.mitre.openid.connect.view.JsonErrorView
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.validation.BeanPropertyBindingResult
+import org.springframework.web.servlet.view.AbstractView
+import java.io.IOException
+import java.io.Writer
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * @author aanganes, jricher
- *
  */
 @Component(JsonErrorView.VIEWNAME)
-public class JsonErrorView extends AbstractView {
+class JsonErrorView : AbstractView() {
+    private val gson: Gson = GsonBuilder()
+        .setExclusionStrategies(object : ExclusionStrategy {
+            override fun shouldSkipField(f: FieldAttributes): Boolean {
+                return false
+            }
+
+            override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                // skip the JPA binding wrapper
+                if (clazz == BeanPropertyBindingResult::class.java) {
+                    return true
+                }
+                return false
+            }
+        })
+        .serializeNulls()
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+        .create()
+
+    override fun renderMergedOutputModel(
+        model: Map<String, Any>,
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
 
 
-	public static final String ERROR_MESSAGE = "errorMessage";
+        var code = model[HttpCodeView.CODE] as HttpStatus?
+        if (code == null) {
+            code = HttpStatus.INTERNAL_SERVER_ERROR // default to 500
+        }
+
+        response.setStatus(code.value())
+
+        try {
+            val out: Writer = response.writer
+
+            var errorTitle = model[ERROR] as String?
+            if (Strings.isNullOrEmpty(errorTitle)) {
+                errorTitle = "mitreid_error"
+            }
+            val errorMessage = model[ERROR_MESSAGE] as String?
+            val obj = JsonObject()
+            obj.addProperty("error", errorTitle)
+            obj.addProperty("error_description", errorMessage)
+            gson.toJson(obj, out)
+        } catch (e: IOException) {
+            Companion.logger.error("IOException in JsonErrorView.java: ", e)
+        }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE: String = "errorMessage"
 
 
-	public static final String ERROR = "error";
+        const val ERROR: String = "error"
 
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(JsonErrorView.class);
+        /**
+         * Logger for this class
+         */
+        private val logger: Logger = LoggerFactory.getLogger(JsonErrorView::class.java)
 
-	public static final String VIEWNAME = "jsonErrorView";
-
-	private Gson gson = new GsonBuilder()
-			.setExclusionStrategies(new ExclusionStrategy() {
-
-				@Override
-				public boolean shouldSkipField(FieldAttributes f) {
-
-					return false;
-				}
-
-				@Override
-				public boolean shouldSkipClass(Class<?> clazz) {
-					// skip the JPA binding wrapper
-					if (clazz.equals(BeanPropertyBindingResult.class)) {
-						return true;
-					}
-					return false;
-				}
-
-			})
-			.serializeNulls()
-			.setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-			.create();
-
-	@Override
-	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) {
-
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-
-		HttpStatus code = (HttpStatus) model.get(HttpCodeView.CODE);
-		if (code == null) {
-			code = HttpStatus.INTERNAL_SERVER_ERROR; // default to 500
-		}
-
-		response.setStatus(code.value());
-
-		try {
-
-			Writer out = response.getWriter();
-
-			String errorTitle = (String) model.get(ERROR);
-			if (Strings.isNullOrEmpty(errorTitle)) {
-				errorTitle = "mitreid_error";
-			}
-			String errorMessage = (String) model.get(ERROR_MESSAGE);
-			JsonObject obj = new JsonObject();
-			obj.addProperty("error", errorTitle);
-			obj.addProperty("error_description", errorMessage);
-			gson.toJson(obj, out);
-
-		} catch (IOException e) {
-
-			logger.error("IOException in JsonErrorView.java: ", e);
-
-		}
-	}
-
+        const val VIEWNAME: String = "jsonErrorView"
+    }
 }
