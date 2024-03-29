d6dbcff0238906ae5d3b8e680fe8845b9fb2c1f5
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 18:05:20 +0000

    Convert ConnectRequestParameters to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/request/ConnectRequestParameters.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/request/ConnectRequestParameters.kt
@@ -5,50 +5,48 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.request;
+ */
+package org.mitre.openid.connect.request
 
-public interface ConnectRequestParameters {
+object ConnectRequestParameters {
+    const val CLIENT_ID: String = "client_id"
+    const val RESPONSE_TYPE: String = "response_type"
+    const val REDIRECT_URI: String = "redirect_uri"
+    const val STATE: String = "state"
+    const val DISPLAY: String = "display"
+    const val REQUEST: String = "request"
+    const val LOGIN_HINT: String = "login_hint"
+    const val MAX_AGE: String = "max_age"
+    const val CLAIMS: String = "claims"
+    const val SCOPE: String = "scope"
+    const val NONCE: String = "nonce"
+    const val PROMPT: String = "prompt"
 
-	public String CLIENT_ID = "client_id";
-	public String RESPONSE_TYPE = "response_type";
-	public String REDIRECT_URI = "redirect_uri";
-	public String STATE = "state";
-	public String DISPLAY = "display";
-	public String REQUEST = "request";
-	public String LOGIN_HINT = "login_hint";
-	public String MAX_AGE = "max_age";
-	public String CLAIMS = "claims";
-	public String SCOPE = "scope";
-	public String NONCE = "nonce";
-	public String PROMPT = "prompt";
+    // prompt values
+    const val PROMPT_LOGIN: String = "login"
+    const val PROMPT_NONE: String = "none"
+    const val PROMPT_CONSENT: String = "consent"
+    const val PROMPT_SEPARATOR: String = " "
 
-	// prompt values
-	public String PROMPT_LOGIN = "login";
-	public String PROMPT_NONE = "none";
-	public String PROMPT_CONSENT = "consent";
-	public String PROMPT_SEPARATOR = " ";
+    // extensions
+    const val APPROVED_SITE: String = "approved_site"
 
-	// extensions
-	public String APPROVED_SITE = "approved_site";
+    // responses
+    const val ERROR: String = "error"
+    const val LOGIN_REQUIRED: String = "login_required"
 
-	// responses
-	public String ERROR = "error";
-	public String LOGIN_REQUIRED = "login_required";
+    // audience
+    const val AUD: String = "aud"
 
-	// audience
-	public String AUD = "aud";
-
-	// PKCE
-	public String CODE_CHALLENGE = "code_challenge";
-	public String CODE_CHALLENGE_METHOD = "code_challenge_method";
-	public String CODE_VERIFIER = "code_verifier";
-
+    // PKCE
+    const val CODE_CHALLENGE: String = "code_challenge"
+    const val CODE_CHALLENGE_METHOD: String = "code_challenge_method"
+    const val CODE_VERIFIER: String = "code_verifier"
 }
