e9cf04e2a42cb63642365e73a285e0cc6543ce0f
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 17:54:28 +0000

    Convert JpaApprovedSiteRepository to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/repository/impl/JpaApprovedSiteRepository.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/repository/impl/JpaApprovedSiteRepository.kt
@@ -7,98 +7,80 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.repository.impl;
+ */
+package org.mitre.openid.connect.repository.impl
 
-import static org.mitre.util.jpa.JpaUtil.saveOrUpdate;
-
-import java.util.Collection;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import org.mitre.openid.connect.model.ApprovedSite;
-import org.mitre.openid.connect.repository.ApprovedSiteRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.mitre.openid.connect.model.ApprovedSite
+import org.mitre.openid.connect.repository.ApprovedSiteRepository
+import org.mitre.util.jpa.JpaUtil.saveOrUpdate
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 /**
  * JPA ApprovedSite repository implementation
  *
  * @author Michael Joseph Walsh, aanganes
- *
  */
 @Repository
-public class JpaApprovedSiteRepository implements ApprovedSiteRepository {
+class JpaApprovedSiteRepository : ApprovedSiteRepository {
+    @PersistenceContext(unitName = "defaultPersistenceUnit")
+    private lateinit var manager: EntityManager
 
-	@PersistenceContext(unitName="defaultPersistenceUnit")
-	private EntityManager manager;
+    @get:Transactional(value = "defaultTransactionManager")
+    override val all: Collection<ApprovedSite>
+        get() {
+            val query = manager.createNamedQuery(ApprovedSite.QUERY_ALL, ApprovedSite::class.java)
+            return query.resultList
+        }
 
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public Collection<ApprovedSite> getAll() {
-		TypedQuery<ApprovedSite> query = manager.createNamedQuery(ApprovedSite.QUERY_ALL, ApprovedSite.class);
-		return query.getResultList();
-	}
+    @Transactional(value = "defaultTransactionManager")
+    override fun getById(id: java.lang.Long): ApprovedSite? {
+        return manager.find(ApprovedSite::class.java, id)
+    }
 
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public ApprovedSite getById(Long id) {
-		return manager.find(ApprovedSite.class, id);
-	}
+    @Transactional(value = "defaultTransactionManager")
+    override fun remove(approvedSite: ApprovedSite) {
+        val found = manager.find(ApprovedSite::class.java, approvedSite.id)
+            ?: throw IllegalArgumentException()
 
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public void remove(ApprovedSite approvedSite) {
-		ApprovedSite found = manager.find(ApprovedSite.class, approvedSite.getId());
+        manager.remove(found)
+    }
 
-		if (found != null) {
-			manager.remove(found);
-		} else {
-			throw new IllegalArgumentException();
-		}
-	}
+    @Transactional(value = "defaultTransactionManager")
+    override fun save(approvedSite: ApprovedSite): ApprovedSite {
+        return saveOrUpdate<ApprovedSite, Long>(approvedSite.id!!, manager, approvedSite)
+    }
 
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public ApprovedSite save(ApprovedSite approvedSite) {
-		return saveOrUpdate(approvedSite.getId(), manager, approvedSite);
-	}
+    override fun getByClientIdAndUserId(clientId: String?, userId: String?): Collection<ApprovedSite>? {
+        val query = manager.createNamedQuery(ApprovedSite.QUERY_BY_CLIENT_ID_AND_USER_ID, ApprovedSite::class.java)
+        query.setParameter(ApprovedSite.PARAM_USER_ID, userId)
+        query.setParameter(ApprovedSite.PARAM_CLIENT_ID, clientId)
 
-	@Override
-	public Collection<ApprovedSite> getByClientIdAndUserId(String clientId, String userId) {
+        return query.resultList
+    }
 
-		TypedQuery<ApprovedSite> query = manager.createNamedQuery(ApprovedSite.QUERY_BY_CLIENT_ID_AND_USER_ID, ApprovedSite.class);
-		query.setParameter(ApprovedSite.PARAM_USER_ID, userId);
-		query.setParameter(ApprovedSite.PARAM_CLIENT_ID, clientId);
+    @Transactional(value = "defaultTransactionManager")
+    override fun getByUserId(userId: String): Collection<ApprovedSite> {
+        val query = manager.createNamedQuery(ApprovedSite.QUERY_BY_USER_ID, ApprovedSite::class.java)
+        query.setParameter(ApprovedSite.PARAM_USER_ID, userId)
 
-		return query.getResultList();
-	}
+        return query.resultList
+    }
 
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public Collection<ApprovedSite> getByUserId(String userId) {
-		TypedQuery<ApprovedSite> query = manager.createNamedQuery(ApprovedSite.QUERY_BY_USER_ID, ApprovedSite.class);
-		query.setParameter(ApprovedSite.PARAM_USER_ID, userId);
+    @Transactional(value = "defaultTransactionManager")
+    override fun getByClientId(clientId: String): Collection<ApprovedSite>? {
+        val query = manager.createNamedQuery(ApprovedSite.QUERY_BY_CLIENT_ID, ApprovedSite::class.java)
+        query.setParameter(ApprovedSite.PARAM_CLIENT_ID, clientId)
 
-		return query.getResultList();
-
-	}
-
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public Collection<ApprovedSite> getByClientId(String clientId) {
-		TypedQuery<ApprovedSite> query = manager.createNamedQuery(ApprovedSite.QUERY_BY_CLIENT_ID, ApprovedSite.class);
-		query.setParameter(ApprovedSite.PARAM_CLIENT_ID, clientId);
-
-		return query.getResultList();
-	}
+        return query.resultList
+    }
 }
