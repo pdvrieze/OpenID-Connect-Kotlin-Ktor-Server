5bd491ad98afaa8c8b6b475d52d485aec8de76d8
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 17:56:16 +0000

    Convert JpaBlacklistedSiteRepository to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/repository/impl/JpaBlacklistedSiteRepository.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/repository/impl/JpaBlacklistedSiteRepository.kt
@@ -7,93 +7,75 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.repository.impl;
+ */
+package org.mitre.openid.connect.repository.impl
 
-import org.mitre.openid.connect.model.BlacklistedSite;
-import org.mitre.openid.connect.repository.BlacklistedSiteRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import java.util.Collection;
-
-import static org.mitre.util.jpa.JpaUtil.saveOrUpdate;
+import org.mitre.openid.connect.model.BlacklistedSite
+import org.mitre.openid.connect.repository.BlacklistedSiteRepository
+import org.mitre.util.jpa.JpaUtil.saveOrUpdate
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 /**
  * @author jricher
- *
  */
 @Repository
-public class JpaBlacklistedSiteRepository implements BlacklistedSiteRepository {
+class JpaBlacklistedSiteRepository : BlacklistedSiteRepository {
+    @PersistenceContext(unitName = "defaultPersistenceUnit")
+    private lateinit var manager: EntityManager
 
-	@PersistenceContext(unitName="defaultPersistenceUnit")
-	private EntityManager manager;
+    @get:Transactional(value = "defaultTransactionManager")
+    override val all: Collection<BlacklistedSite>
+        get() {
+            val query = manager.createNamedQuery(BlacklistedSite.QUERY_ALL, BlacklistedSite::class.java)
+            return query.resultList
+        }
 
-	/* (non-Javadoc)
-	 * @see org.mitre.openid.connect.repository.BlacklistedSiteRepository#getAll()
-	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public Collection<BlacklistedSite> getAll() {
-		TypedQuery<BlacklistedSite> query = manager.createNamedQuery(BlacklistedSite.QUERY_ALL, BlacklistedSite.class);
-		return query.getResultList();
-	}
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.repository.BlacklistedSiteRepository#getById(java.lang.Long)
 	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public BlacklistedSite getById(Long id) {
-		return manager.find(BlacklistedSite.class, id);
-	}
+    @Transactional(value = "defaultTransactionManager")
+    override fun getById(id: java.lang.Long): BlacklistedSite {
+        return manager.find(BlacklistedSite::class.java, id)
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.repository.BlacklistedSiteRepository#remove(org.mitre.openid.connect.model.BlacklistedSite)
 	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public void remove(BlacklistedSite blacklistedSite) {
-		BlacklistedSite found = manager.find(BlacklistedSite.class, blacklistedSite.getId());
+    @Transactional(value = "defaultTransactionManager")
+    override fun remove(blacklistedSite: BlacklistedSite) {
+        val found = manager.find(BlacklistedSite::class.java, blacklistedSite.id)
 
-		if (found != null) {
-			manager.remove(found);
-		} else {
-			throw new IllegalArgumentException();
-		}
+        if (found != null) {
+            manager.remove(found)
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
 
-	}
-
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.repository.BlacklistedSiteRepository#save(org.mitre.openid.connect.model.BlacklistedSite)
 	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public BlacklistedSite save(BlacklistedSite blacklistedSite) {
-		return saveOrUpdate(blacklistedSite.getId(), manager, blacklistedSite);
-	}
+    @Transactional(value = "defaultTransactionManager")
+    override fun save(blacklistedSite: BlacklistedSite): BlacklistedSite {
+        return saveOrUpdate(blacklistedSite.id!!, manager, blacklistedSite)
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
 	 * @see org.mitre.openid.connect.repository.BlacklistedSiteRepository#update(org.mitre.openid.connect.model.BlacklistedSite, org.mitre.openid.connect.model.BlacklistedSite)
 	 */
-	@Override
-	@Transactional(value="defaultTransactionManager")
-	public BlacklistedSite update(BlacklistedSite oldBlacklistedSite, BlacklistedSite blacklistedSite) {
-
-		blacklistedSite.setId(oldBlacklistedSite.getId());
-		return saveOrUpdate(oldBlacklistedSite.getId(), manager, blacklistedSite);
-
-	}
-
+    @Transactional(value = "defaultTransactionManager")
+    override fun update(oldBlacklistedSite: BlacklistedSite, blacklistedSite: BlacklistedSite): BlacklistedSite {
+        blacklistedSite.id = oldBlacklistedSite.id
+        return saveOrUpdate(oldBlacklistedSite.id!!, manager, blacklistedSite)
+    }
 }
