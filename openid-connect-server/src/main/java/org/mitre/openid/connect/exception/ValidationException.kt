86ae36d27d3b92d7683317f896f34ee44c75a2e5
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 17:40:59 +0000

    Convert ValidationException to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/exception/ValidationException.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/exception/ValidationException.kt
@@ -5,58 +5,33 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.exception;
+ */
+package org.mitre.openid.connect.exception
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus
 
 /**
  * Thrown by utility methods when a client fails to validate. Contains information
  * to be returned.
  * @author jricher
- *
  */
-public class ValidationException extends Exception {
-	private static final long serialVersionUID = 1820497072989294627L;
+class ValidationException(
+    var error: String,
+    var errorDescription: String,
+    var status: HttpStatus
+) : Exception() {
+    override fun toString(): String {
+        return "ValidationException [error=$error, errorDescription=$errorDescription, status=$status]"
+    }
 
-	private String error;
-	private String errorDescription;
-	private HttpStatus status;
-	public ValidationException(String error, String errorDescription,
-			HttpStatus status) {
-		this.error = error;
-		this.errorDescription = errorDescription;
-		this.status = status;
-	}
-	public String getError() {
-		return error;
-	}
-	public void setError(String error) {
-		this.error = error;
-	}
-	public String getErrorDescription() {
-		return errorDescription;
-	}
-	public void setErrorDescription(String errorDescription) {
-		this.errorDescription = errorDescription;
-	}
-	public HttpStatus getStatus() {
-		return status;
-	}
-	public void setStatus(HttpStatus status) {
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return "ValidationException [error=" + error + ", errorDescription="
-				+ errorDescription + ", status=" + status + "]";
-	}
+    companion object {
+        private const val serialVersionUID = 1820497072989294627L
+    }
 }
