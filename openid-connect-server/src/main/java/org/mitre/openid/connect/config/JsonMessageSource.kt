d32a7138f5b63349c0d99d9ebae7732a6c3dda7a
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-29 17:37:04 +0000

    Convert JsonMessageSource to Kotlin.

--- openid-connect-server/src/main/java/org/mitre/openid/connect/config/JsonMessageSource.kt
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/config/JsonMessageSource.kt
@@ -5,187 +5,149 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.openid.connect.config
 
-package org.mitre.openid.connect.config;
-
-import com.google.common.base.Splitter;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.AbstractMessageSource;
-import org.springframework.core.io.Resource;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import com.google.common.base.Splitter
+import com.google.gson.JsonElement
+import com.google.gson.JsonIOException
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.JsonSyntaxException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.support.AbstractMessageSource
+import org.springframework.core.io.Resource
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStreamReader
+import java.text.MessageFormat
+import java.util.*
 
 /**
  * @author jricher
  */
-public class JsonMessageSource extends AbstractMessageSource {
+@Component
+class JsonMessageSource : AbstractMessageSource() {
+    var baseDirectory: Resource? = null
 
-	private static final Logger logger = LoggerFactory.getLogger(JsonMessageSource.class);
+    private val fallbackLocale = Locale("en") // US English is the fallback language
 
-	private Resource baseDirectory;
+    private val languageMaps: MutableMap<Locale, List<JsonObject>?> = HashMap()
 
-	private Locale fallbackLocale = new Locale("en"); // US English is the fallback language
+    @Autowired
+    private lateinit var config: ConfigurationPropertiesBean
 
-	private Map<Locale, List<JsonObject>> languageMaps = new HashMap<>();
+    public override fun resolveCode(code: String, locale: Locale): MessageFormat? {
+        val value = getValue(code, getLanguageMap(locale))
+                // if we haven't found anything, try the default locale
+            ?: getValue(code, getLanguageMap(fallbackLocale))
+            ?: return null // if it's still null, return null
 
-	@Autowired
-	private ConfigurationPropertiesBean config;
+        return MessageFormat(value, locale)
+    }
 
-	@Override
-	protected MessageFormat resolveCode(String code, Locale locale) {
+    /**
+     * Get a value from the set of maps, taking the first match in order
+     */
+    private fun getValue(code: String, langs: List<JsonObject>?): String? {
+        return langs?.run { asSequence().mapNotNull { getValue(code, it) }.firstOrNull() }
+    }
 
-		List<JsonObject> langs = getLanguageMap(locale);
+    /**
+     * Get a value from a single map
+     */
+    private fun getValue(code: String, lang: JsonObject?): String? {
+        // if there's no language map, nothing to look up
 
-		String value = getValue(code, langs);
+        if (lang == null) {
+            return null
+        }
 
-		if (value == null) {
-			// if we haven't found anything, try the default locale
-			langs = getLanguageMap(fallbackLocale);
-			value = getValue(code, langs);
-		}
+        var e: JsonElement = lang
 
-		if (value == null) {
-			// if it's still null, return null
-			return null;
-		} else {
-			// otherwise format the message
-			return new MessageFormat(value, locale);
-		}
+        val parts = Splitter.on('.').split(code)
+        val it: Iterator<String> = parts.iterator()
 
-	}
+        var value: String? = null
 
-	/**
-	 * Get a value from the set of maps, taking the first match in order
-	 */
-	private String getValue(String code, List<JsonObject> langs) {
-		if (langs == null || langs.isEmpty()) {
-			// no language maps, nothing to look up
-			return null;
-		}
+        while (it.hasNext()) {
+            val p = it.next()
+            if (e.isJsonObject) {
+                val o = e.asJsonObject
+                if (o.has(p)) {
+                    e = o[p] // found the next level
+                    if (!it.hasNext()) {
+                        // we've reached a leaf, grab it
+                        if (e.isJsonPrimitive) {
+                            value = e.asString
+                        }
+                    }
+                } else {
+                    // didn't find it, stop processing
+                    break
+                }
+            } else {
+                // didn't find it, stop processing
+                break
+            }
+        }
 
-		for (JsonObject lang : langs) {
-			String value = getValue(code, lang);
-			if (value != null) {
-				// short circuit out of here if we find a match, otherwise keep going through the list
-				return value;
-			}
-		}
-
-		// if we didn't find anything return null
-		return null;
-	}
-
-	/**
-	 * Get a value from a single map
-	 */
-	private String getValue(String code, JsonObject lang) {
-
-		// if there's no language map, nothing to look up
-		if (lang == null) {
-			return null;
-		}
-
-		JsonElement e = lang;
-
-		Iterable<String> parts = Splitter.on('.').split(code);
-		Iterator<String> it = parts.iterator();
-
-		String value = null;
-
-		while (it.hasNext()) {
-			String p = it.next();
-			if (e.isJsonObject()) {
-				JsonObject o = e.getAsJsonObject();
-				if (o.has(p)) {
-					e = o.get(p); // found the next level
-					if (!it.hasNext()) {
-						// we've reached a leaf, grab it
-						if (e.isJsonPrimitive()) {
-							value = e.getAsString();
-						}
-					}
-				} else {
-					// didn't find it, stop processing
-					break;
-				}
-			} else {
-				// didn't find it, stop processing
-				break;
-			}
-		}
-
-		return value;
-	}
+        return value
+    }
 
 
-	private List<JsonObject> getLanguageMap(Locale locale) {
+    private fun getLanguageMap(locale: Locale): List<JsonObject>? {
+        if (!languageMaps.containsKey(locale)) {
+            try {
+                val set: MutableList<JsonObject> = ArrayList()
+                for (namespace in config.languageNamespaces!!) {
+                    // full locale string, e.g. "en_US"
+                    var filename = "${locale.language}_${locale.country}${File.separator}$namespace.json"
 
-		if (!languageMaps.containsKey(locale)) {
-			try {
-				List<JsonObject> set = new ArrayList<>();
-				for (String namespace : config.getLanguageNamespaces()) {
-					// full locale string, e.g. "en_US"
-					String filename = locale.getLanguage() + "_" + locale.getCountry() + File.separator + namespace + ".json";
+                    var r = baseDirectory!!.createRelative(filename)
 
-					Resource r = getBaseDirectory().createRelative(filename);
+                    if (!r.exists()) {
+                        // fallback to language only
+                        logger.debug("Fallback locale to language only.")
+                        filename = locale.language + File.separator + namespace + ".json"
+                        r = baseDirectory!!.createRelative(filename)
+                    }
 
-					if (!r.exists()) {
-						// fallback to language only
-						logger.debug("Fallback locale to language only.");
-						filename = locale.getLanguage() + File.separator + namespace + ".json";
-						r = getBaseDirectory().createRelative(filename);
-					}
+                    Companion.logger.info("No locale loaded, trying to load from {}", r)
 
-					logger.info("No locale loaded, trying to load from {}", r);
+                    val parser = JsonParser()
+                    val obj = parser.parse(InputStreamReader(r.inputStream, "UTF-8")) as JsonObject
 
-					JsonParser parser = new JsonParser();
-					JsonObject obj = (JsonObject) parser.parse(new InputStreamReader(r.getInputStream(), "UTF-8"));
+                    set.add(obj)
+                }
+                languageMaps[locale] = set
+            } catch (e: FileNotFoundException) {
+                Companion.logger.info("Unable to load locale because no messages file was found for locale {}", locale.displayName)
+                languageMaps[locale] = null
+            } catch (e: JsonIOException) {
+                Companion.logger.error("Unable to load locale", e)
+            } catch (e: JsonSyntaxException) {
+                Companion.logger.error("Unable to load locale", e)
+            } catch (e: IOException) {
+                Companion.logger.error("Unable to load locale", e)
+            }
+        }
 
-					set.add(obj);
-				}
-				languageMaps.put(locale, set);
-			} catch (FileNotFoundException e) {
-				logger.info("Unable to load locale because no messages file was found for locale {}", locale.getDisplayName());
-				languageMaps.put(locale, null);
-			} catch (JsonIOException | JsonSyntaxException | IOException e) {
-				logger.error("Unable to load locale", e);
-			}
-		}
+        return languageMaps[locale]
+    }
 
-		return languageMaps.get(locale);
-	}
-
-	public Resource getBaseDirectory() {
-		return baseDirectory;
-	}
-
-	public void setBaseDirectory(Resource baseDirectory) {
-		this.baseDirectory = baseDirectory;
-	}
-
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(JsonMessageSource::class.java)
+    }
 }
