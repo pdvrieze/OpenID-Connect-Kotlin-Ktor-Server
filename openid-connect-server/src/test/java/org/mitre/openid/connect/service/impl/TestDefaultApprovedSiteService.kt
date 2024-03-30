571c9bb93705ffa45f67ed94a1a67de773b3743a
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 17:17:54 +0000

    Convert TestDefaultApprovedSiteService to Kotlin.

--- openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestDefaultApprovedSiteService.kt
+++ openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestDefaultApprovedSiteService.kt
@@ -7,126 +7,120 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.service.impl;
+ */
+package org.mitre.openid.connect.service.impl
 
-import static org.mockito.ArgumentMatchers.any;
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mitre.oauth2.model.ClientDetailsEntity
+import org.mitre.oauth2.repository.OAuth2TokenRepository
+import org.mitre.openid.connect.model.ApprovedSite
+import org.mitre.openid.connect.repository.ApprovedSiteRepository
+import org.mitre.openid.connect.service.ApprovedSiteService
+import org.mitre.openid.connect.service.StatsService
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.isA
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.test.annotation.Rollback
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+@RunWith(MockitoJUnitRunner::class)
+class TestDefaultApprovedSiteService {
+    private lateinit var site1: ApprovedSite
+    private lateinit var site2: ApprovedSite
+    private lateinit var site3: ApprovedSite
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mitre.oauth2.model.ClientDetailsEntity;
-import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
-import org.mitre.oauth2.repository.OAuth2TokenRepository;
-import org.mitre.openid.connect.model.ApprovedSite;
-import org.mitre.openid.connect.repository.ApprovedSiteRepository;
-import org.mitre.openid.connect.service.ApprovedSiteService;
-import org.mitre.openid.connect.service.StatsService;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.annotation.Rollback;
+    private lateinit var client: ClientDetailsEntity
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
+    @Mock
+    private lateinit var repository: ApprovedSiteRepository
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
+    @Mock
+    private lateinit var tokenRepository: OAuth2TokenRepository
 
-@RunWith(MockitoJUnitRunner.class)
-public class TestDefaultApprovedSiteService {
+    @Mock
+    private lateinit var statsService: StatsService
 
-	private ApprovedSite site1;
-	private ApprovedSite site2;
-	private ApprovedSite site3;
-
-	private ClientDetailsEntity client;
-	private final String clientId = "client";
-
-	@Mock
-	private ApprovedSiteRepository repository;
-
-	@Mock
-	private OAuth2TokenRepository tokenRepository;
-
-	@Mock
-	private StatsService statsService;
-
-	@InjectMocks
-	private ApprovedSiteService service = new DefaultApprovedSiteService();
+    @InjectMocks
+    private val service: ApprovedSiteService = DefaultApprovedSiteService()
 
 
-	/**
-	 * Initialize the service and repository mock. Initialize a client and
-	 * several ApprovedSite objects for use in unit tests.
-	 */
-	@Before
-	public void prepare() {
+    /**
+     * Initialize the service and repository mock. Initialize a client and
+     * several ApprovedSite objects for use in unit tests.
+     */
+    @Before
+    fun prepare() {
+        client = ClientDetailsEntity().also {
+            it.clientId = clientId
+        }
 
-		client = new ClientDetailsEntity();
-		client.setClientId(clientId);
+        site1 = ApprovedSite().apply {
+            id = 1L
+            userId = "user1"
+            clientId = "other"
+        }
 
-		site1 = new ApprovedSite();
-		site1.setId(1L);
-		site1.setUserId("user1");
-		site1.setClientId("other");
+        site2 = ApprovedSite().also {
+            it.id = 2L
+            it.userId = "user1"
+            it.clientId = clientId
+        }
 
-		site2 = new ApprovedSite();
-		site2.setId(2L);
-		site2.setUserId("user1");
-		site2.setClientId(clientId);
+        site3 = ApprovedSite().also {
+            it.id = 3L
+            it.userId = "user2"
+            it.clientId = clientId
+        }
 
-		site3 = new ApprovedSite();
-		site3.setId(3L);
-		site3.setUserId("user2");
-		site3.setClientId(clientId);
+        Mockito.reset(repository, statsService)
+    }
 
-		Mockito.reset(repository, statsService);
+    /**
+     * Test clearing approved sites for a client that has 2 stored approved sites.
+     * Ensure that the repository's remove() method is called twice.
+     */
+    @Test
+    fun clearApprovedSitesForClient_success() {
+        whenever(repository.getByClientId(client.clientId!!))
+            .thenReturn(setOf(site2, site3))
 
-	}
+        whenever(tokenRepository.getAccessTokensForApprovedSite(isA<ApprovedSite>()))
+            .thenReturn(emptyList())
 
-	/**
-	 * Test clearing approved sites for a client that has 2 stored approved sites.
-	 * Ensure that the repository's remove() method is called twice.
-	 */
-	@Test
-	public void clearApprovedSitesForClient_success() {
-		Set<ApprovedSite> setToReturn = Sets.newHashSet(site2, site3);
-		Mockito.when(repository.getByClientId(client.getClientId())).thenReturn(setToReturn);
-		List<OAuth2AccessTokenEntity> tokens = ImmutableList.of();
-		Mockito.when(tokenRepository.getAccessTokensForApprovedSite(any(ApprovedSite.class))).thenReturn(tokens);
+        service.clearApprovedSitesForClient(client)
 
-		service.clearApprovedSitesForClient(client);
+        verify(repository, times(2)).remove(isA<ApprovedSite>())
+    }
 
-		Mockito.verify(repository, times(2)).remove(any(ApprovedSite.class));
-	}
-
-	/**
-	 * Test clearing approved sites for a client that doesn't have any stored approved
-	 * sites. Ensure that the repository's remove() method is never called in this case.
-	 */
-	@Test
-	@Rollback
-	public void clearApprovedSitesForClient_null() {
-		String otherId = "a different id";
-		client.setClientId(otherId);
-		service.clearApprovedSitesForClient(client);
-		// unused by mockito (causs unnecessary stubbing exception
+    /**
+     * Test clearing approved sites for a client that doesn't have any stored approved
+     * sites. Ensure that the repository's remove() method is never called in this case.
+     */
+    @Test
+    @Rollback
+    fun clearApprovedSitesForClient_null() {
+        val otherId = "a different id"
+        client.clientId = otherId
+        service.clearApprovedSitesForClient(client)
+        // unused by mockito (causs unnecessary stubbing exception
 //		Mockito.when(repository.getByClientId(otherId)).thenReturn(new HashSet<ApprovedSite>());
-		Mockito.verify(repository, never()).remove(any(ApprovedSite.class));
-	}
+        Mockito.verify(repository, Mockito.never()).remove(isA<ApprovedSite>())
+    }
 
-
+    companion object {
+        private val clientId = "client"
+    }
 }
