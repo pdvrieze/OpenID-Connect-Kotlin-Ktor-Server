16b481a61ffbb7ff5c45cfbd0a5610b955d457c3
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 22:07:29 +0000

    Convert TestUUIDPairwiseIdentiferService to Kotlin.

--- openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestUUIDPairwiseIdentiferService.kt
+++ openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestUUIDPairwiseIdentiferService.kt
@@ -7,191 +7,191 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.service.impl;
+ */
+package org.mitre.openid.connect.service.impl
 
-import com.google.common.collect.ImmutableSet;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mitre.oauth2.model.ClientDetailsEntity;
-import org.mitre.oauth2.model.ClientDetailsEntity.SubjectType;
-import org.mitre.openid.connect.model.DefaultUserInfo;
-import org.mitre.openid.connect.model.PairwiseIdentifier;
-import org.mitre.openid.connect.model.UserInfo;
-import org.mitre.openid.connect.repository.PairwiseIdentifierRepository;
-import org.mitre.openid.connect.service.PairwiseIdentiferService;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.Set;
-import java.util.UUID;
-
-import static org.junit.Assert.*;
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotSame
+import org.junit.runner.RunWith
+import org.mitre.oauth2.model.ClientDetailsEntity
+import org.mitre.oauth2.model.ClientDetailsEntity.SubjectType
+import org.mitre.openid.connect.model.DefaultUserInfo
+import org.mitre.openid.connect.model.PairwiseIdentifier
+import org.mitre.openid.connect.model.UserInfo
+import org.mitre.openid.connect.repository.PairwiseIdentifierRepository
+import org.mitre.openid.connect.service.PairwiseIdentiferService
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.atLeast
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.util.*
 
 /**
  * @author jricher
- *
  */
-@RunWith(MockitoJUnitRunner.class)
-public class TestUUIDPairwiseIdentiferService {
+@RunWith(MockitoJUnitRunner::class)
+class TestUUIDPairwiseIdentiferService {
+    @Mock
+    private lateinit var pairwiseIdentifierRepository: PairwiseIdentifierRepository
 
-	@Mock
-	private PairwiseIdentifierRepository pairwiseIdentifierRepository;
+    @InjectMocks
+    private lateinit var service: UUIDPairwiseIdentiferService
 
-	@InjectMocks
-	private UUIDPairwiseIdentiferService service;
+    private lateinit var userInfoRegular: UserInfo
 
-	private UserInfo userInfoRegular;
+    private lateinit var pairwiseClient1: ClientDetailsEntity
+    private lateinit var pairwiseClient2: ClientDetailsEntity
+    private lateinit var pairwiseClient3: ClientDetailsEntity
+    private lateinit var pairwiseClient4: ClientDetailsEntity
+    private lateinit var pairwiseClient5: ClientDetailsEntity
 
-	private ClientDetailsEntity pairwiseClient1;
-	private ClientDetailsEntity pairwiseClient2;
-	private ClientDetailsEntity pairwiseClient3;
-	private ClientDetailsEntity pairwiseClient4;
-	private ClientDetailsEntity pairwiseClient5;
 
-	private String regularUsername = "regular";
-	private String regularSub = "regularSub652ha23b";
-	private String pairwiseSub = "pairwise-12-regular-user";
+    private lateinit var savedPairwiseIdentifier: PairwiseIdentifier
 
-	private String pairwiseClientId1 = "pairwiseClient-1-2312";
-	private String pairwiseClientId2 = "pairwiseClient-2-324416";
-	private String pairwiseClientId3 = "pairwiseClient-3-154157";
-	private String pairwiseClientId4 = "pairwiseClient-4-4589723";
-	private String pairwiseClientId5 = "pairwiseClient-5-34908713";
+    @Before
+    fun prepare() {
+        userInfoRegular = DefaultUserInfo()
+        userInfoRegular.preferredUsername = regularUsername
+        userInfoRegular.sub = regularSub
 
-	private String sectorHost12 = "sector-identifier-12";
-	private String sectorHost3 = "sector-identifier-3";
-	private String clientHost4 = "client-redirect-4";
-	private String clientHost5 = "client-redirect-5";
+        // pairwise set 1
+        pairwiseClient1 = ClientDetailsEntity()
+        pairwiseClient1.clientId = pairwiseClientId1
+        pairwiseClient1.subjectType = SubjectType.PAIRWISE
+        pairwiseClient1.sectorIdentifierUri = sectorIdentifier1
 
-	private String sectorIdentifier1 = "https://" + sectorHost12 + "/url";
-	private String sectorIdentifier2 = "https://" + sectorHost12 + "/url2";
-	private String sectorIdentifier3 = "https://" + sectorHost3 + "/url";
+        pairwiseClient2 = ClientDetailsEntity()
+        pairwiseClient2.clientId = pairwiseClientId2
+        pairwiseClient2.subjectType = SubjectType.PAIRWISE
+        pairwiseClient2.sectorIdentifierUri = sectorIdentifier2
 
-	private Set<String> pairwiseClient3RedirectUris = ImmutableSet.of("https://" + sectorHost3 + "/oauth", "https://" + sectorHost3 + "/other");
-	private Set<String> pairwiseClient4RedirectUris = ImmutableSet.of("https://" + clientHost4 + "/oauth");
-	private Set<String> pairwiseClient5RedirectUris = ImmutableSet.of("https://" + clientHost5 + "/oauth", "https://" + clientHost5 + "/other");
+        // pairwise set 2
+        pairwiseClient3 = ClientDetailsEntity()
+        pairwiseClient3.clientId = pairwiseClientId3
+        pairwiseClient3.subjectType = SubjectType.PAIRWISE
+        pairwiseClient3.sectorIdentifierUri = sectorIdentifier3
+        pairwiseClient3.redirectUris = pairwiseClient3RedirectUris
 
-	private PairwiseIdentifier savedPairwiseIdentifier;
+        // pairwise with null sector
+        pairwiseClient4 = ClientDetailsEntity()
+        pairwiseClient4.clientId = pairwiseClientId4
+        pairwiseClient4.subjectType = SubjectType.PAIRWISE
+        pairwiseClient4.redirectUris = pairwiseClient4RedirectUris
 
-	@Before
-	public void prepare() {
-		userInfoRegular = new DefaultUserInfo();
-		userInfoRegular.setPreferredUsername(regularUsername);
-		userInfoRegular.setSub(regularSub);
+        // pairwise with multiple redirects and no sector (error)
+        pairwiseClient5 = ClientDetailsEntity()
+        pairwiseClient5.clientId = pairwiseClientId5
+        pairwiseClient5.subjectType = SubjectType.PAIRWISE
+        pairwiseClient5.redirectUris = pairwiseClient5RedirectUris
 
-		// pairwise set 1
-		pairwiseClient1 = new ClientDetailsEntity();
-		pairwiseClient1.setClientId(pairwiseClientId1);
-		pairwiseClient1.setSubjectType(SubjectType.PAIRWISE);
-		pairwiseClient1.setSectorIdentifierUri(sectorIdentifier1);
+        // saved pairwise identifier from repository
+        savedPairwiseIdentifier = PairwiseIdentifier()
+        savedPairwiseIdentifier.userSub = regularSub
+        savedPairwiseIdentifier.identifier = pairwiseSub
+        savedPairwiseIdentifier.sectorIdentifier = sectorHost12
+    }
 
-		pairwiseClient2 = new ClientDetailsEntity();
-		pairwiseClient2.setClientId(pairwiseClientId2);
-		pairwiseClient2.setSubjectType(SubjectType.PAIRWISE);
-		pairwiseClient2.setSectorIdentifierUri(sectorIdentifier2);
+    /**
+     * Test method for [PairwiseIdentiferService.getIdentifier].
+     */
+    @Test
+    fun testGetIdentifier_existingEqual() {
+        whenever(pairwiseIdentifierRepository.getBySectorIdentifier(regularSub, sectorHost12))
+            .thenReturn(savedPairwiseIdentifier)
 
-		// pairwise set 2
-		pairwiseClient3 = new ClientDetailsEntity();
-		pairwiseClient3.setClientId(pairwiseClientId3);
-		pairwiseClient3.setSubjectType(SubjectType.PAIRWISE);
-		pairwiseClient3.setSectorIdentifierUri(sectorIdentifier3);
-		pairwiseClient3.setRedirectUris(pairwiseClient3RedirectUris);
+        val pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1)
+        val pairwise2 = service.getIdentifier(userInfoRegular, pairwiseClient2)
 
-		// pairwise with null sector
-		pairwiseClient4 = new ClientDetailsEntity();
-		pairwiseClient4.setClientId(pairwiseClientId4);
-		pairwiseClient4.setSubjectType(SubjectType.PAIRWISE);
-		pairwiseClient4.setRedirectUris(pairwiseClient4RedirectUris);
+        assertEquals(pairwiseSub, pairwise1)
+        assertEquals(pairwiseSub, pairwise2)
+    }
 
-		// pairwise with multiple redirects and no sector (error)
-		pairwiseClient5 = new ClientDetailsEntity();
-		pairwiseClient5.setClientId(pairwiseClientId5);
-		pairwiseClient5.setSubjectType(SubjectType.PAIRWISE);
-		pairwiseClient5.setRedirectUris(pairwiseClient5RedirectUris);
+    @Test
+    fun testGetIdentifier_newEqual() {
+        val pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1)
+        verify(pairwiseIdentifierRepository, atLeast(1))
+            .save(any<PairwiseIdentifier>())
 
-		// saved pairwise identifier from repository
-		savedPairwiseIdentifier = new PairwiseIdentifier();
-		savedPairwiseIdentifier.setUserSub(regularSub);
-		savedPairwiseIdentifier.setIdentifier(pairwiseSub);
-		savedPairwiseIdentifier.setSectorIdentifier(sectorHost12);
+        val pairwiseId = PairwiseIdentifier()
+        pairwiseId.userSub = regularSub
+        pairwiseId.identifier = pairwise1
+        pairwiseId.sectorIdentifier = sectorHost12
 
-	}
+        whenever(pairwiseIdentifierRepository.getBySectorIdentifier(regularSub, sectorHost12))
+            .thenReturn(pairwiseId)
 
-	/**
-	 * Test method for {@link PairwiseIdentiferService#getIdentifier(UserInfo, ClientDetailsEntity)}.
-	 */
-	@Test
-	public void testGetIdentifier_existingEqual() {
+        val pairwise2 = service.getIdentifier(userInfoRegular, pairwiseClient2)
 
-		Mockito.when(pairwiseIdentifierRepository.getBySectorIdentifier(regularSub, sectorHost12)).thenReturn(savedPairwiseIdentifier);
+        assertNotSame(pairwiseSub, pairwise1)
+        assertNotSame(pairwiseSub, pairwise2)
 
-		String pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1);
-		String pairwise2 = service.getIdentifier(userInfoRegular, pairwiseClient2);
+        assertEquals(pairwise1, pairwise2)
 
-		assertEquals(pairwiseSub, pairwise1);
-		assertEquals(pairwiseSub, pairwise2);
+        // see if the pairwise id's are actual UUIDs
+        UUID.fromString(pairwise1)
+        UUID.fromString(pairwise2)
+    }
 
-	}
+    @Test
+    fun testGetIdentifer_unique() {
+        val pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1)
+        val pairwise3 = service.getIdentifier(userInfoRegular, pairwiseClient3)
+        val pairwise4 = service.getIdentifier(userInfoRegular, pairwiseClient4)
 
-	@Test
-	public void testGetIdentifier_newEqual() {
+        // make sure nothing's equal
+        assertNotSame(pairwise1, pairwise3)
+        assertNotSame(pairwise1, pairwise4)
+        assertNotSame(pairwise3, pairwise4)
 
-		String pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1);
-		Mockito.verify(pairwiseIdentifierRepository, Mockito.atLeast(1)).save(ArgumentMatchers.any(PairwiseIdentifier.class));
+        // see if the pairwise id's are actual UUIDs
+        UUID.fromString(pairwise1)
+        UUID.fromString(pairwise3)
+        UUID.fromString(pairwise4)
+    }
 
-		PairwiseIdentifier pairwiseId = new PairwiseIdentifier();
-		pairwiseId.setUserSub(regularSub);
-		pairwiseId.setIdentifier(pairwise1);
-		pairwiseId.setSectorIdentifier(sectorHost12);
+    @Test(expected = IllegalArgumentException::class)
+    fun testGetIdentifier_multipleRedirectError() {
+        service.getIdentifier(userInfoRegular, pairwiseClient5)
+    }
 
-		Mockito.when(pairwiseIdentifierRepository.getBySectorIdentifier(regularSub, sectorHost12)).thenReturn(pairwiseId);
+    companion object {
+        private const val regularUsername = "regular"
+        private const val regularSub = "regularSub652ha23b"
+        private const val pairwiseSub = "pairwise-12-regular-user"
 
-		String pairwise2 = service.getIdentifier(userInfoRegular, pairwiseClient2);
+        private const val pairwiseClientId1 = "pairwiseClient-1-2312"
+        private const val pairwiseClientId2 = "pairwiseClient-2-324416"
+        private const val pairwiseClientId3 = "pairwiseClient-3-154157"
+        private const val pairwiseClientId4 = "pairwiseClient-4-4589723"
+        private const val pairwiseClientId5 = "pairwiseClient-5-34908713"
 
-		assertNotSame(pairwiseSub, pairwise1);
-		assertNotSame(pairwiseSub, pairwise2);
+        private const val sectorHost12 = "sector-identifier-12"
+        private const val sectorHost3 = "sector-identifier-3"
+        private const val clientHost4 = "client-redirect-4"
+        private const val clientHost5 = "client-redirect-5"
 
-		assertEquals(pairwise1, pairwise2);
+        private const val sectorIdentifier1 = "https://$sectorHost12/url"
+        private const val sectorIdentifier2 = "https://$sectorHost12/url2"
+        private const val sectorIdentifier3 = "https://$sectorHost3/url"
 
-		// see if the pairwise id's are actual UUIDs
-		UUID uudi1 = UUID.fromString(pairwise1);
-		UUID uuid2 = UUID.fromString(pairwise2);
+        private val pairwiseClient3RedirectUris: Set<String> =
+            setOf("https://$sectorHost3/oauth", "https://$sectorHost3/other")
+        private val pairwiseClient4RedirectUris: Set<String> = setOf("https://$clientHost4/oauth")
+        private val pairwiseClient5RedirectUris: Set<String> =
+            setOf("https://$clientHost5/oauth", "https://$clientHost5/other")
 
-	}
-
-	@Test
-	public void testGetIdentifer_unique() {
-		String pairwise1 = service.getIdentifier(userInfoRegular, pairwiseClient1);
-		String pairwise3 = service.getIdentifier(userInfoRegular, pairwiseClient3);
-		String pairwise4 = service.getIdentifier(userInfoRegular, pairwiseClient4);
-
-		// make sure nothing's equal
-		assertNotSame(pairwise1, pairwise3);
-		assertNotSame(pairwise1, pairwise4);
-		assertNotSame(pairwise3, pairwise4);
-
-		// see if the pairwise id's are actual UUIDs
-		UUID uudi1 = UUID.fromString(pairwise1);
-		UUID uudi3 = UUID.fromString(pairwise3);
-		UUID uudi4 = UUID.fromString(pairwise4);
-
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testGetIdentifier_multipleRedirectError() {
-		String pairwise5 = service.getIdentifier(userInfoRegular, pairwiseClient5);
-	}
+    }
 
 }
