a19bf6254c88ea3a2fcd3cfa6e58830da154e209
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 17:22:08 +0000

    Convert TestDefaultApprovedSiteService to Kotlin.

--- openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestDefaultBlacklistedSiteService.kt
+++ openid-connect-server/src/test/java/org/mitre/openid/connect/service/impl/TestDefaultBlacklistedSiteService.kt
@@ -7,97 +7,80 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package org.mitre.openid.connect.service.impl;
+ */
+package org.mitre.openid.connect.service.impl
 
-import java.util.Set;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mitre.openid.connect.model.BlacklistedSite;
-import org.mitre.openid.connect.repository.BlacklistedSiteRepository;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import com.google.common.collect.Sets;
-
-import static org.mockito.Mockito.times;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mitre.openid.connect.model.BlacklistedSite
+import org.mitre.openid.connect.repository.BlacklistedSiteRepository
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 /**
  * @author wkim
- *
  */
-@RunWith(MockitoJUnitRunner.class)
-public class TestDefaultBlacklistedSiteService {
+@RunWith(MockitoJUnitRunner::class)
+class TestDefaultBlacklistedSiteService {
+    private lateinit var site1: BlacklistedSite
+    private lateinit var site2: BlacklistedSite
 
-	private BlacklistedSite site1;
-	private BlacklistedSite site2;
+    private lateinit var blackListedSitesSet: Set<BlacklistedSite>
 
-	private String uri1 = "black1";
-	private String uri2 = "black2";
-	private String uri3 = "not-black";
+    @Mock
+    private lateinit var mockRepository: BlacklistedSiteRepository
 
-	private Set<BlacklistedSite> blackListedSitesSet;
+    @InjectMocks
+    private val service = DefaultBlacklistedSiteService()
 
-	@Mock
-	private BlacklistedSiteRepository mockRepository;
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    @Throws(Exception::class)
+    fun prepare() {
+        site1 = BlacklistedSite().apply { uri = uri1 }
+        site2 = BlacklistedSite().apply { uri = uri2 }
 
-	@InjectMocks
-	private DefaultBlacklistedSiteService service = new DefaultBlacklistedSiteService();
+        blackListedSitesSet = setOf(site1, site2)
+    }
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void prepare() throws Exception {
+    @Test
+    fun isBlacklisted_yes(): Unit {
+        whenever(mockRepository.all).thenReturn(blackListedSitesSet)
 
-		site1 = new BlacklistedSite();
-		site2 = new BlacklistedSite();
+        assertTrue(service.isBlacklisted(uri1))
+        assertTrue(service.isBlacklisted(uri2))
 
-		site1.setUri(uri1);
-		site2.setUri(uri2);
+        verify(mockRepository, times(2)).all
+    }
 
-		blackListedSitesSet = Sets.newHashSet(site1, site2);
-	}
+    @Test
+    fun isBlacklisted_no(): Unit {
+        whenever(mockRepository.all).thenReturn(blackListedSitesSet)
 
-	/**
-	 * Test finding blacklisted sites from the repository.
-	 */
-	@Test
-	public void isBlacklisted_yes() {
+        assertFalse(service.isBlacklisted(uri3))
 
-		Mockito.when(mockRepository.getAll()).thenReturn(blackListedSitesSet);
+        verify(mockRepository).all
+    }
 
-		assertTrue(service.isBlacklisted(uri1));
-		assertTrue(service.isBlacklisted(uri2));
-
-		Mockito.verify(mockRepository, times(2)).getAll();
-	}
-
-	/**
-	 * Tests for finding a site that is not blacklisted in the repository.
-	 */
-	@Test
-	public void isBlacklisted_no() {
-
-		Mockito.when(mockRepository.getAll()).thenReturn(blackListedSitesSet);
-
-		assertFalse(service.isBlacklisted(uri3));
-
-		Mockito.verify(mockRepository).getAll();
-	}
-
+    companion object {
+        private val uri1 = "black1"
+        private val uri2 = "black2"
+        private val uri3 = "not-black"
+    }
 }
