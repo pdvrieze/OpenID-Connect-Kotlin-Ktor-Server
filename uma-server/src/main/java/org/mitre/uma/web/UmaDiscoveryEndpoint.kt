3f21b0307b5e59b33721feeda965ef5505d57842
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 23:24:03 +0000

    Convert UmaDiscoveryEndpoint to Kotlin.

--- uma-server/src/main/java/org/mitre/uma/web/UmaDiscoveryEndpoint.kt
+++ uma-server/src/main/java/org/mitre/uma/web/UmaDiscoveryEndpoint.kt
@@ -5,75 +5,65 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.uma.web
 
-package org.mitre.uma.web;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.mitre.oauth2.web.IntrospectionEndpoint;
-import org.mitre.openid.connect.config.ConfigurationPropertiesBean;
-import org.mitre.openid.connect.view.JsonEntityView;
-import org.mitre.openid.connect.web.DynamicClientRegistrationEndpoint;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableSet
+import com.google.common.collect.Lists
+import org.mitre.oauth2.web.IntrospectionEndpoint
+import org.mitre.openid.connect.config.ConfigurationPropertiesBean
+import org.mitre.openid.connect.view.JsonEntityView
+import org.mitre.openid.connect.web.DynamicClientRegistrationEndpoint
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
 
 /**
  * @author jricher
- *
  */
 @Controller
-public class UmaDiscoveryEndpoint {
+class UmaDiscoveryEndpoint {
+    @Autowired
+    private lateinit var config: ConfigurationPropertiesBean
 
-	@Autowired
-	private ConfigurationPropertiesBean config;
+    @RequestMapping(".well-known/uma-configuration")
+    fun umaConfiguration(model: Model): String {
 
-	@RequestMapping(".well-known/uma-configuration")
-	public String umaConfiguration(Model model) {
+        val issuer = config.issuer
+        val tokenProfiles = ImmutableSet.of("bearer")
+        val grantTypes =
+            Lists.newArrayList("authorization_code", "implicit", "urn:ietf:params:oauth:grant-type:jwt-bearer", "client_credentials", "urn:ietf:params:oauth:grant_type:redelegate")
 
-		Map<String, Object> m = new HashMap<>();
-
-		String issuer = config.getIssuer();
-		ImmutableSet<String> tokenProfiles = ImmutableSet.of("bearer");
-		ArrayList<String> grantTypes = Lists.newArrayList("authorization_code", "implicit", "urn:ietf:params:oauth:grant-type:jwt-bearer", "client_credentials", "urn:ietf:params:oauth:grant_type:redelegate");
-
-		m.put("version", "1.0");
-		m.put("issuer", issuer);
-		m.put("pat_profiles_supported", tokenProfiles);
-		m.put("aat_profiles_supported", tokenProfiles);
-		m.put("rpt_profiles_supported", tokenProfiles);
-		m.put("pat_grant_types_supported", grantTypes);
-		m.put("aat_grant_types_supported", grantTypes);
-		m.put("claim_token_profiles_supported", ImmutableSet.of());
-		m.put("uma_profiles_supported", ImmutableSet.of());
-		m.put("dynamic_client_endpoint", issuer + DynamicClientRegistrationEndpoint.URL);
-		m.put("token_endpoint", issuer + "token");
-		m.put("authorization_endpoint", issuer + "authorize");
-		m.put("requesting_party_claims_endpoint", issuer + ClaimsCollectionEndpoint.URL);
-		m.put("introspection_endpoint", issuer + IntrospectionEndpoint.URL);
-		m.put("resource_set_registration_endpoint", issuer + ResourceSetRegistrationEndpoint.DISCOVERY_URL);
-		m.put("permission_registration_endpoint", issuer + PermissionRegistrationEndpoint.URL);
-		m.put("rpt_endpoint", issuer + AuthorizationRequestEndpoint.URL);
-
-
-
-		model.addAttribute("entity", m);
-		return JsonEntityView.VIEWNAME;
-	}
+        val m = mapOf(
+            "version" to "1.0",
+            "issuer" to issuer,
+            "pat_profiles_supported" to tokenProfiles,
+            "aat_profiles_supported" to tokenProfiles,
+            "rpt_profiles_supported" to tokenProfiles,
+            "pat_grant_types_supported" to grantTypes,
+            "aat_grant_types_supported" to grantTypes,
+            "claim_token_profiles_supported" to setOf<Any>(),
+            "uma_profiles_supported" to setOf<Any>(),
+            "dynamic_client_endpoint" to "$issuer${DynamicClientRegistrationEndpoint.URL}",
+            "token_endpoint" to "${issuer}token",
+            "authorization_endpoint" to "${issuer}authorize",
+            "requesting_party_claims_endpoint" to "$issuer${ClaimsCollectionEndpoint.URL}",
+            "introspection_endpoint" to issuer + IntrospectionEndpoint.URL,
+            "resource_set_registration_endpoint" to "$issuer${ResourceSetRegistrationEndpoint.DISCOVERY_URL}",
+            "permission_registration_endpoint" to "$issuer${PermissionRegistrationEndpoint.URL}",
+            "rpt_endpoint" to issuer + AuthorizationRequestEndpoint.URL,
+        )
 
 
+        model.addAttribute("entity", m)
+        return JsonEntityView.VIEWNAME
+    }
 }
