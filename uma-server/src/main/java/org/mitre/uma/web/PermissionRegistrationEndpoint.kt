d7b9fe9502afe7a73dd2baf021baacfbf858d8a4
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 23:01:33 +0000

    Convert PermissionRegistrationEndpoint to Kotlin.

--- uma-server/src/main/java/org/mitre/uma/web/PermissionRegistrationEndpoint.kt
+++ uma-server/src/main/java/org/mitre/uma/web/PermissionRegistrationEndpoint.kt
@@ -5,151 +5,141 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.uma.web
 
-package org.mitre.uma.web;
-
-import static org.mitre.oauth2.web.AuthenticationUtilities.ensureOAuthScope;
-import static org.mitre.util.JsonUtils.getAsLong;
-import static org.mitre.util.JsonUtils.getAsStringSet;
-
-import java.util.Set;
-
-import org.mitre.oauth2.model.SystemScope;
-import org.mitre.oauth2.service.SystemScopeService;
-import org.mitre.openid.connect.view.JsonEntityView;
-import org.mitre.openid.connect.view.JsonErrorView;
-import org.mitre.uma.model.PermissionTicket;
-import org.mitre.uma.model.ResourceSet;
-import org.mitre.uma.service.PermissionService;
-import org.mitre.uma.service.ResourceSetService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonObject
+import com.google.gson.JsonParseException
+import com.google.gson.JsonParser
+import org.mitre.oauth2.model.SystemScope
+import org.mitre.oauth2.service.SystemScopeService
+import org.mitre.oauth2.util.toJavaId
+import org.mitre.oauth2.web.AuthenticationUtilities.ensureOAuthScope
+import org.mitre.openid.connect.view.JsonEntityView
+import org.mitre.openid.connect.view.JsonErrorView
+import org.mitre.uma.service.PermissionService
+import org.mitre.uma.service.ResourceSetService
+import org.mitre.util.JsonUtils.getAsLong
+import org.mitre.util.JsonUtils.getAsStringSet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.util.MimeTypeUtils
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 /**
  * @author jricher
- *
  */
 @Controller
 @RequestMapping("/" + PermissionRegistrationEndpoint.URL)
 @PreAuthorize("hasRole('ROLE_USER')")
-public class PermissionRegistrationEndpoint {
-	// Logger for this class
-	private static final Logger logger = LoggerFactory.getLogger(PermissionRegistrationEndpoint.class);
+class PermissionRegistrationEndpoint {
+    @Autowired
+    private lateinit var permissionService: PermissionService
 
-	public static final String URL = "permission";
+    @Autowired
+    private lateinit var resourceSetService: ResourceSetService
 
-	@Autowired
-	private PermissionService permissionService;
+    @Autowired
+    private lateinit var scopeService: SystemScopeService
 
-	@Autowired
-	private ResourceSetService resourceSetService;
+    private val parser = JsonParser()
 
-	@Autowired
-	private SystemScopeService scopeService;
+    @RequestMapping(method = [RequestMethod.POST], consumes = [MimeTypeUtils.APPLICATION_JSON_VALUE], produces = [MimeTypeUtils.APPLICATION_JSON_VALUE])
+    fun getPermissionTicket(@RequestBody jsonString: String?, m: Model, auth: Authentication): String {
+        ensureOAuthScope(auth, SystemScopeService.UMA_PROTECTION_SCOPE)
 
-	private JsonParser parser = new JsonParser();
+        try {
+            // parse the permission request
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MimeTypeUtils.APPLICATION_JSON_VALUE, produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-	public String getPermissionTicket(@RequestBody String jsonString, Model m, Authentication auth) {
+            val el = parser.parse(jsonString)
+            if (el.isJsonObject) {
+                val o = el.asJsonObject
 
-		ensureOAuthScope(auth, SystemScopeService.UMA_PROTECTION_SCOPE);
+                val rsid = getAsLong(o, "resource_set_id")
+                var scopes: Set<String>? = getAsStringSet(o, "scopes")
 
-		try {
+                if (rsid == null || scopes.isNullOrEmpty()) {
+                    // missing information
+                    m.addAttribute("code", HttpStatus.BAD_REQUEST)
+                    m.addAttribute("errorMessage", "Missing required component of permission registration request.")
+                    return JsonErrorView.VIEWNAME
+                }
 
-			// parse the permission request
+                // trim any restricted scopes
+                val scopesRequested: Set<SystemScope> =
+                    scopeService.removeRestrictedAndReservedScopes(scopeService.fromStrings(scopes)!!)
 
-			JsonElement el = parser.parse(jsonString);
-			if (el.isJsonObject()) {
-				JsonObject o = el.getAsJsonObject();
+                scopes = scopeService.toStrings(scopesRequested)
 
-				Long rsid = getAsLong(o, "resource_set_id");
-				Set<String> scopes = getAsStringSet(o, "scopes");
+                val resourceSet = resourceSetService.getById(rsid.toJavaId())
 
-				if (rsid == null || scopes == null || scopes.isEmpty()){
-					// missing information
-					m.addAttribute("code", HttpStatus.BAD_REQUEST);
-					m.addAttribute("errorMessage", "Missing required component of permission registration request.");
-					return JsonErrorView.VIEWNAME;
-				}
+                // requested resource set doesn't exist
+                if (resourceSet == null) {
+                    m.addAttribute("code", HttpStatus.NOT_FOUND)
+                    m.addAttribute("errorMessage", "Requested resource set not found: $rsid")
+                    return JsonErrorView.VIEWNAME
+                }
 
-				// trim any restricted scopes
-				Set<SystemScope> scopesRequested = scopeService.fromStrings(scopes);
-				scopesRequested = scopeService.removeRestrictedAndReservedScopes(scopesRequested);
-				scopes = scopeService.toStrings(scopesRequested);
+                // authorized user of the token doesn't match owner of the resource set
+                if (resourceSet.owner != auth.name) {
+                    m.addAttribute("code", HttpStatus.FORBIDDEN)
+                    m.addAttribute("errorMessage", "Party requesting permission is not owner of resource set, expected " + resourceSet.owner + " got " + auth.name)
+                    return JsonErrorView.VIEWNAME
+                }
 
-				ResourceSet resourceSet = resourceSetService.getById(rsid);
+                // create the permission
+                val permission = permissionService.createTicket(resourceSet, scopes!!)
 
-				// requested resource set doesn't exist
-				if (resourceSet == null) {
-					m.addAttribute("code", HttpStatus.NOT_FOUND);
-					m.addAttribute("errorMessage", "Requested resource set not found: " + rsid);
-					return JsonErrorView.VIEWNAME;
-				}
+                if (permission != null) {
+                    // we've created the permission, return the ticket
+                    val out = JsonObject()
+                    out.addProperty("ticket", permission.ticket)
+                    m.addAttribute("entity", out)
 
-				// authorized user of the token doesn't match owner of the resource set
-				if (!resourceSet.getOwner().equals(auth.getName())) {
-					m.addAttribute("code", HttpStatus.FORBIDDEN);
-					m.addAttribute("errorMessage", "Party requesting permission is not owner of resource set, expected " + resourceSet.getOwner() + " got " + auth.getName());
-					return JsonErrorView.VIEWNAME;
-				}
+                    m.addAttribute("code", HttpStatus.CREATED)
 
-				// create the permission
-				PermissionTicket permission = permissionService.createTicket(resourceSet, scopes);
+                    return JsonEntityView.VIEWNAME
+                } else {
+                    // there was a failure creating the permission object
 
-				if (permission != null) {
-					// we've created the permission, return the ticket
-					JsonObject out = new JsonObject();
-					out.addProperty("ticket", permission.getTicket());
-					m.addAttribute("entity", out);
+                    m.addAttribute("code", HttpStatus.INTERNAL_SERVER_ERROR)
+                    m.addAttribute("errorMessage", "Unable to save permission and generate ticket.")
 
-					m.addAttribute("code", HttpStatus.CREATED);
+                    return JsonErrorView.VIEWNAME
+                }
+            } else {
+                // malformed request
+                m.addAttribute("code", HttpStatus.BAD_REQUEST)
+                m.addAttribute("errorMessage", "Malformed JSON request.")
+                return JsonErrorView.VIEWNAME
+            }
+        } catch (e: JsonParseException) {
+            // malformed request
+            m.addAttribute("code", HttpStatus.BAD_REQUEST)
+            m.addAttribute("errorMessage", "Malformed JSON request.")
+            return JsonErrorView.VIEWNAME
+        }
+    }
 
-					return JsonEntityView.VIEWNAME;
-				} else {
-					// there was a failure creating the permission object
+    companion object {
+        // Logger for this class
+        private val logger: Logger = LoggerFactory.getLogger(PermissionRegistrationEndpoint::class.java)
 
-					m.addAttribute("code", HttpStatus.INTERNAL_SERVER_ERROR);
-					m.addAttribute("errorMessage", "Unable to save permission and generate ticket.");
-
-					return JsonErrorView.VIEWNAME;
-				}
-
-			} else {
-				// malformed request
-				m.addAttribute("code", HttpStatus.BAD_REQUEST);
-				m.addAttribute("errorMessage", "Malformed JSON request.");
-				return JsonErrorView.VIEWNAME;
-			}
-		} catch (JsonParseException e) {
-			// malformed request
-			m.addAttribute("code", HttpStatus.BAD_REQUEST);
-			m.addAttribute("errorMessage", "Malformed JSON request.");
-			return JsonErrorView.VIEWNAME;
-		}
-
-	}
-
+        const val URL: String = "permission"
+    }
 }
