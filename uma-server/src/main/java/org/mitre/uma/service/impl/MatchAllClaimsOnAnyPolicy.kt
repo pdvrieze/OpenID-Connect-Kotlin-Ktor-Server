2882ca8ed914717c9139a172bec44fdcfe69aa2f
Paul de Vrieze <paul.devrieze@gmail.com>
2024-03-30 22:43:28 +0000

    Convert MatchAllClaimsOnAnyPolicy to Kotlin.

--- uma-server/src/main/java/org/mitre/uma/service/impl/MatchAllClaimsOnAnyPolicy.kt
+++ uma-server/src/main/java/org/mitre/uma/service/impl/MatchAllClaimsOnAnyPolicy.kt
@@ -5,85 +5,73 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ */
+package org.mitre.uma.service.impl
 
-package org.mitre.uma.service.impl;
-
-import java.util.Collection;
-import java.util.HashSet;
-
-import org.mitre.uma.model.Claim;
-import org.mitre.uma.model.ClaimProcessingResult;
-import org.mitre.uma.model.PermissionTicket;
-import org.mitre.uma.model.Policy;
-import org.mitre.uma.model.ResourceSet;
-import org.mitre.uma.service.ClaimsProcessingService;
-import org.springframework.stereotype.Service;
+import org.mitre.uma.model.Claim
+import org.mitre.uma.model.ClaimProcessingResult
+import org.mitre.uma.model.PermissionTicket
+import org.mitre.uma.model.ResourceSet
+import org.mitre.uma.service.ClaimsProcessingService
+import org.springframework.stereotype.Service
 
 /**
  * Tests if all the claims in the required set have a matching
  * value in the supplied set.
  *
  * @author jricher
- *
  */
 @Service("matchAllClaimsOnAnyPolicy")
-public class MatchAllClaimsOnAnyPolicy implements ClaimsProcessingService {
-
-	/* (non-Javadoc)
+class MatchAllClaimsOnAnyPolicy : ClaimsProcessingService {
+    /* (non-Javadoc)
 	 * @see org.mitre.uma.service.ClaimsProcessingService#claimsAreSatisfied(java.util.Collection, java.util.Collection)
 	 */
-	@Override
-	public ClaimProcessingResult claimsAreSatisfied(ResourceSet rs, PermissionTicket ticket) {
-		Collection<Claim> allUnmatched = new HashSet<>();
-		for (Policy policy : rs.getPolicies()) {
-			Collection<Claim> unmatched = checkIndividualClaims(policy.getClaimsRequired(), ticket.getClaimsSupplied());
-			if (unmatched.isEmpty()) {
-				// we found something that's satisfied the claims, let's go with it!
-				return new ClaimProcessingResult(policy);
-			} else {
-				// otherwise add it to the stack to send back
-				allUnmatched.addAll(unmatched);
-			}
-		}
+    override fun claimsAreSatisfied(rs: ResourceSet, ticket: PermissionTicket): ClaimProcessingResult {
+        val allUnmatched: MutableCollection<Claim> = HashSet()
+        for (policy in rs.policies!!) {
+            val unmatched = checkIndividualClaims(policy.claimsRequired!!, ticket.claimsSupplied!!)
+            if (unmatched.isEmpty()) {
+                // we found something that's satisfied the claims, let's go with it!
+                return ClaimProcessingResult(policy)
+            } else {
+                // otherwise add it to the stack to send back
+                allUnmatched.addAll(unmatched)
+            }
+        }
 
-		// otherwise, tell the caller that we'll need some set of these fulfilled somehow
-		return new ClaimProcessingResult(allUnmatched);
-	}
+        // otherwise, tell the caller that we'll need some set of these fulfilled somehow
+        return ClaimProcessingResult(allUnmatched)
+    }
 
-	private Collection<Claim> checkIndividualClaims(Collection<Claim> claimsRequired, Collection<Claim> claimsSupplied) {
+    private fun checkIndividualClaims(
+        claimsRequired: Collection<Claim>,
+        claimsSupplied: Collection<Claim>
+    ): Collection<Claim> {
+        val claimsUnmatched: MutableCollection<Claim> = claimsRequired.toHashSet()
 
-		Collection<Claim> claimsUnmatched = new HashSet<>(claimsRequired);
+        // see if each of the required claims has a counterpart in the supplied claims set
+        for (required in claimsRequired) {
+            for (supplied in claimsSupplied) {
+                if (required.issuer!!.containsAll(supplied.issuer!!)) {
+                    // it's from the right issuer
 
-		// see if each of the required claims has a counterpart in the supplied claims set
-		for (Claim required : claimsRequired) {
-			for (Claim supplied : claimsSupplied) {
+                    if (required.name == supplied.name && required.value == supplied.value) {
+                        // the claim matched, pull it from the set
 
-				if (required.getIssuer().containsAll(supplied.getIssuer())) {
-					// it's from the right issuer
+                        claimsUnmatched.remove(required)
+                    }
+                }
+            }
+        }
 
-					if (required.getName().equals(supplied.getName()) &&
-							required.getValue().equals(supplied.getValue())) {
-
-						// the claim matched, pull it from the set
-						claimsUnmatched.remove(required);
-
-					}
-
-				}
-			}
-		}
-
-		// if there's anything left then the claims aren't satisfied, return the leftovers
-		return claimsUnmatched;
-
-	}
-
+        // if there's anything left then the claims aren't satisfied, return the leftovers
+        return claimsUnmatched
+    }
 }
