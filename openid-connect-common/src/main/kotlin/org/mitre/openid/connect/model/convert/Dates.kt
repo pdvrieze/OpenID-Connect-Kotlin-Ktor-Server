ac04c4d66110f16f5e39b99868834fbe25e8fecc
Paul de Vrieze <paul.devrieze@gmail.com>
2024-09-13 20:43:29 +0100

    Use proper formatter for ISO dates that will render to 3 digits of miliseconds.

--- openid-connect-common/src/main/kotlin/org/mitre/openid/connect/model/convert/Dates.kt
+++ openid-connect-common/src/main/kotlin/org/mitre/openid/connect/model/convert/Dates.kt
@@ -10,6 +10,8 @@ import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
 import java.util.*
 
 typealias ISODate = @Serializable(IsoDateSerializer::class) Date
@@ -18,13 +20,31 @@ object IsoDateSerializer : KSerializer<Date> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("java.util.Date.ISO", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Date) {
-        encoder.encodeString(dateFormatter.format(value.toInstant()))
+        encoder.encodeString(dateFormatter.format(value.toInstant().atZone(UTC_ZONE)))
     }
 
     override fun deserialize(decoder: Decoder): Date {
         return Date.from(Instant.from(dateFormatter.parse(decoder.decodeString())))
     }
 
-    private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME.withLocale(Locale.ENGLISH).withZone(ZoneId.of("UTC"))
+    private val dateFormatter: DateTimeFormatter = DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .append(DateTimeFormatter.ISO_LOCAL_DATE)
+        .appendLiteral('T')
+        .appendValue(ChronoField.HOUR_OF_DAY, 2)
+        .appendLiteral(':')
+        .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+        .optionalStart()
+        .appendLiteral(':')
+        .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+        .parseLenient()
+        .optionalStart()
+        .appendFraction(ChronoField.NANO_OF_SECOND, 3, 3, true)
+        .optionalEnd()
+        .appendOffsetId()
+        .parseStrict()
+        .toFormatter()
+
+    private val UTC_ZONE = ZoneId.of("UTC")
 
 }
