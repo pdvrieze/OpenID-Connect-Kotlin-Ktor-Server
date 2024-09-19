907bd4ea68eaef2fa27592041a17c4e4f9099a28
Paul de Vrieze <paul.devrieze@gmail.com>
2024-09-19 20:28:06 +0100

    Don't duplicate the sidebar.

--- openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/views/about.kt
+++ openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/views/about.kt
@@ -1,7 +1,6 @@
 package io.github.pdvrieze.openid.web.views
 
 import io.github.pdvrieze.openid.web.WebContext
-import io.github.pdvrieze.openid.web.tags.sidebar
 import kotlinx.html.TagConsumer
 import kotlinx.html.div
 import kotlinx.html.h2
@@ -10,9 +9,7 @@ import kotlinx.html.p
 // TODO: highlight proper section of topbar; what is the right way to do this?
 fun <T, C : TagConsumer<T>> C.about(context: WebContext): T {
     val title = context.intl.messageText("about.title")
-    return baseView(context, title, "About", true) {
-        sidebar(context)
-
+    return baseView(context, title, "About", hasSideBar = true) {
         div("span10") {
             comment("Main hero unit for a primary marketing message or call to action")
             div("hero-unit") {
