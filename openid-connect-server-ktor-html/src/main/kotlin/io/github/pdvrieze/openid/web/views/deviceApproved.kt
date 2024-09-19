24667e32cef19810b99615d087a894cfb2f0280f
Paul de Vrieze <paul.devrieze@gmail.com>
2024-09-19 21:40:41 +0100

    Finish converting deviceApproved

---
+++ openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/views/deviceApproved.kt
@@ -0,0 +1,45 @@
+import io.github.pdvrieze.openid.web.WebContext
+import io.github.pdvrieze.openid.web.tags.formattedPage
+import io.github.pdvrieze.openid.web.tags.topBar
+import kotlinx.html.TagConsumer
+import kotlinx.html.div
+import kotlinx.html.h1
+import kotlinx.html.i
+import kotlinx.html.style
+import org.mitre.oauth2.model.OAuthClientDetails
+
+fun <T, C : TagConsumer<T>> C.deviceApproved(
+    context: WebContext,
+    client: OAuthClientDetails,
+    approved: Boolean,
+): T {
+    val title = context.intl.messageText("approve.title")
+    val clientName = client.clientName?.takeUnless { it.isBlank() }
+    val clientId = client.clientId
+    return formattedPage(context, title) {
+        with(context.intl) {
+            topBar(context, "Approve")
+            div("container main") {
+                div(classes="well") {
+                    style = "text-align: center"
+                    h1 { +(clientName ?: clientId!!) }
+
+                    when {
+                        approved -> div(classes = "text-success") {
+                            i(classes = "icon-ok")
+                            message("device.approve.approved")
+                        }
+
+                        else -> div(classes = "text-error") {
+                            i(classes = "icon-remove")
+                            message("device.approve.notApproved")
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
