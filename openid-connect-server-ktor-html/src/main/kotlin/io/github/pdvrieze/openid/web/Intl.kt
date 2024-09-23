1e7a9d66ac2d392807c560f6bb1e268ac43d9412
Paul de Vrieze <paul.devrieze@gmail.com>
2024-09-23 18:08:15 +0100

    Broaden the applicability of Intl.message

--- openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/Intl.kt
+++ openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/Intl.kt
@@ -1,12 +1,12 @@
 package io.github.pdvrieze.openid.web
 
-import kotlinx.html.HTMLTag
-import kotlinx.html.unsafe
+import kotlinx.html.HtmlBlockTag
 
 interface Intl {
-    fun HTMLTag.message(key: String, vararg args: Any?) {
-        // use unsafe as the text may contain html elements
-        unsafe { raw(messageText(key, args)) }
+    fun HtmlBlockTag.message(key: String, vararg args: Any?) {
+        // use unsafe as the text may contain html elements. Note that the .unsafe function doesn't work as that
+        // requires an Html tag
+        consumer.onTagContentUnsafe { raw(messageText(key, args)) }
     }
     fun messageText(key: String, vararg args: Any?): String
 }
--- openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/tags/Copyright.kt
+++ openid-connect-server-ktor-html/src/main/kotlin/io/github/pdvrieze/openid/web/tags/Copyright.kt
@@ -2,13 +2,14 @@ package io.github.pdvrieze.openid.web.tags
 
 import io.github.pdvrieze.openid.web.WebContext
 import kotlinx.html.FlowOrPhrasingContent
+import kotlinx.html.HtmlBlockInlineTag
 import kotlinx.html.HtmlTagMarker
 import kotlinx.html.img
 import kotlinx.html.span
 import kotlinx.html.title
 
 @HtmlTagMarker
-fun FlowOrPhrasingContent.copyright(context: WebContext) {
+fun HtmlBlockInlineTag.copyright(context: WebContext) {
     if (context.config.isHeartMode) {
         span("pull-left") {
             img(src="resources/images/heart_mode.png", alt="HEART Mode"){ title="This server is running in HEART Compliance Mode" }
