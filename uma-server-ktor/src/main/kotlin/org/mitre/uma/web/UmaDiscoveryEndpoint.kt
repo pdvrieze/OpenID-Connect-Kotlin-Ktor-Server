480e5f09f85e3a712e2b02aa80751eae93c76233
Paul de Vrieze <paul.devrieze@gmail.com>
2024-09-26 20:58:20 +0100

    Convert the UmaDiscoveryEndpoint

--- uma-server-ktor/src/main/kotlin/org/mitre/uma/web/UmaDiscoveryEndpoint.kt
+++ uma-server-ktor/src/main/kotlin/org/mitre/uma/web/UmaDiscoveryEndpoint.kt
@@ -15,53 +15,60 @@
  */
 package org.mitre.uma.web
 
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.util.pipeline.*
+import kotlinx.serialization.json.addAll
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonArray
+import org.mitre.oauth2.view.respondJson
 import org.mitre.oauth2.web.IntrospectionEndpoint
 import org.mitre.openid.connect.config.ConfigurationPropertiesBean
 import org.mitre.openid.connect.view.JsonEntityView
 import org.mitre.openid.connect.web.DynamicClientRegistrationEndpoint
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.mitre.web.util.KtorEndpoint
+import org.mitre.web.util.config
 
 /**
  * @author jricher
  */
-@Controller
-class UmaDiscoveryEndpoint {
-    @Autowired
-    private lateinit var config: ConfigurationPropertiesBean
+//@Controller
+class UmaDiscoveryEndpoint : KtorEndpoint {
 
-    @RequestMapping(".well-known/uma-configuration")
-    fun umaConfiguration(model: Model): String {
+    override fun Route.addRoutes() {
+        get("/.well-known/uma-configuration") { umaConfiguration()}
+    }
 
+    //    @RequestMapping(".well-known/uma-configuration")
+    suspend fun PipelineContext<Unit, ApplicationCall>.umaConfiguration() {
+        val config = config
         val issuer = config.issuer
         val tokenProfiles = setOf("bearer")
         val grantTypes =
             listOf("authorization_code", "implicit", "urn:ietf:params:oauth:grant-type:jwt-bearer", "client_credentials", "urn:ietf:params:oauth:grant_type:redelegate")
 
-        val m = mapOf(
-            "version" to "1.0",
-            "issuer" to issuer,
-            "pat_profiles_supported" to tokenProfiles,
-            "aat_profiles_supported" to tokenProfiles,
-            "rpt_profiles_supported" to tokenProfiles,
-            "pat_grant_types_supported" to grantTypes,
-            "aat_grant_types_supported" to grantTypes,
-            "claim_token_profiles_supported" to setOf<Any>(),
-            "uma_profiles_supported" to setOf<Any>(),
-            "dynamic_client_endpoint" to "$issuer${DynamicClientRegistrationEndpoint.URL}",
-            "token_endpoint" to "${issuer}token",
-            "authorization_endpoint" to "${issuer}authorize",
-            "requesting_party_claims_endpoint" to "$issuer${ClaimsCollectionEndpoint.URL}",
-            "introspection_endpoint" to issuer + IntrospectionEndpoint.URL,
-            "resource_set_registration_endpoint" to "$issuer${ResourceSetRegistrationEndpoint.DISCOVERY_URL}",
-            "permission_registration_endpoint" to "$issuer${PermissionRegistrationEndpoint.URL}",
-            "rpt_endpoint" to issuer + org.mitre.uma.web.AuthorizationRequestEndpoint.URL,
-        )
-
-
-        model.addAttribute("entity", m)
-        return JsonEntityView.VIEWNAME
+        val m = buildJsonObject {
+            put("version", "1.0")
+            put("issuer", issuer)
+            putJsonArray("pat_profiles_supported") { addAll(tokenProfiles) }
+            putJsonArray("aat_profiles_supported") { addAll(tokenProfiles) }
+            putJsonArray("rpt_profiles_supported") { addAll(tokenProfiles) }
+            putJsonArray("pat_grant_types_supported") { addAll(grantTypes) }
+            putJsonArray("aat_grant_types_supported") { addAll(grantTypes) }
+            putJsonArray("claim_token_profiles_supported") { }
+            putJsonArray("uma_profiles_supported") { }
+            put("dynamic_client_endpoint", "$issuer${DynamicClientRegistrationEndpoint.URL}")
+            put("token_endpoint", "${issuer}token")
+            put("authorization_endpoint", "${issuer}authorize")
+            put("requesting_party_claims_endpoint", "$issuer${ClaimsCollectionEndpoint.URL}")
+            put("introspection_endpoint", issuer + IntrospectionEndpoint.URL)
+            put("resource_set_registration_endpoint", "$issuer${ResourceSetRegistrationEndpoint.DISCOVERY_URL}")
+            put("permission_registration_endpoint", "$issuer${PermissionRegistrationEndpoint.URL}")
+            put("rpt_endpoint", issuer + AuthorizationRequestEndpoint.URL)
+        }
+        return call.respondJson(m)
     }
 }
