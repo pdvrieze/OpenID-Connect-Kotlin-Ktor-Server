30770d2f1bb238c4cfab091a6a557e8ab5176a32
Paul de Vrieze <paul.devrieze@gmail.com>
2024-10-04 21:29:53 +0100

    Properly mock the requests for the Authentication filter.

--- openid-connect-client/src/test/kotlin/org/mitre/openid/connect/client/TestOIDCAuthenticationFilter.kt
+++ openid-connect-client/src/test/kotlin/org/mitre/openid/connect/client/TestOIDCAuthenticationFilter.kt
@@ -1,12 +1,16 @@
 package org.mitre.openid.connect.client
 
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 class TestOIDCAuthenticationFilter {
     private val config: OIDCAuthenticationProvider.Config = mock {  }
@@ -15,11 +19,19 @@ class TestOIDCAuthenticationFilter {
     @Test
     @Throws(Exception::class)
     fun attemptAuthentication_error() {
-        val context = mock<AuthenticationContext>()
-//        val request = mock<HttpServletRequest>()
-//        whenever(request.getParameter("error")).thenReturn("Error")
-//        whenever(request.getParameter("error_description")).thenReturn("Description")
-//        whenever(request.getParameter("error_uri")).thenReturn("http://example.com")
+        val request = mock<ApplicationRequest> {
+            whenever(mock.queryParameters).thenReturn(parameters {
+                append("error", "Error")
+                append("error_description", "Description")
+                append("error_uri", "http://example.com")
+            })
+        }
+        val call = mock<ApplicationCall> {
+            whenever(mock.request).thenReturn(request)
+        }
+        val context = mock<AuthenticationContext> {
+            whenever(mock.call).thenReturn(call)
+        }
 
         val exception = assertThrows<AuthorizationEndpointException> {
             runBlocking {
